d7926b71969d00c57bf10497fbefaf4c23f07e4c
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-07 02:39:09 +1000

    bump

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,24 +19,32 @@ return {
                   ["<C-k>"] = actions.move_selection_previous, -- Move to prev result in listing
                   ["<C-j>"] = actions.move_selection_next, -- Move to next res
                   ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
                }
             }
          }
       })
-        -- Which-Key reigster for group
-        local wk = require("which-key") 
-        wk.register({
-            f = {
-                name = "Find(Telescope)", -- optional group name
-                f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-                r = { "<cmd>Telescope oldfiles<cr>", "Find Recents", noremap = true}, -- additional options for creating the keymap
-                b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
-                g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-                h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-                j = { "<cmd>Telescope jumplist<cr>", "Jumplist" },
-                z = { "<cmd>Telescope colorscheme<cr>", "Temp Colorscheme" },
-            },
-        }, { prefix = "<leader>" })
-    end,
+
+      telescope.load_extension("fzf");
+
+      -- local map = vim.keymap.set
+      -- map("n", "<fc>", "<cmd>Telescope grep_string<cr>", { desc = "Find Current Word" })
+
+
+      -- Which-Key reigster for group
+      local wk = require("which-key") 
+      wk.register({
+         f = {
+            name = "Find(Telescope)", -- optional group name
+            f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+            r = { "<cmd>Telescope oldfiles<cr>", "Find Recents", noremap = true}, -- additional options for creating the keymap
+            b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
+            g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+            h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+            j = { "<cmd>Telescope jumplist<cr>", "Jumplist" },
+            z = { "<cmd>Telescope colorscheme<cr>", "Temp Colorscheme" },
+            c = { "<cmd>Telescope grep_string<cr>", "Find Current Word" },
+
+         },
+      }, { prefix = "<leader>" })
+   end,
 }
