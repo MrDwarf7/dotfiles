b8eabbe1b0e6e8a329620cd7b07360e222f0030b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-07 10:07:36 +1000

    Bump

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -29,22 +29,5 @@ return {
       -- local map = vim.keymap.set
       -- map("n", "<fc>", "<cmd>Telescope grep_string<cr>", { desc = "Find Current Word" })
 
-
-      -- Which-Key reigster for group
-      local wk = require("which-key") 
-      wk.register({
-         f = {
-            name = "Find(Telescope)", -- optional group name
-            f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-            r = { "<cmd>Telescope oldfiles<cr>", "Find Recents", noremap = true}, -- additional options for creating the keymap
-            b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
-            g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-            h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-            j = { "<cmd>Telescope jumplist<cr>", "Jumplist" },
-            z = { "<cmd>Telescope colorscheme<cr>", "Temp Colorscheme" },
-            c = { "<cmd>Telescope grep_string<cr>", "Find Current Word" },
-
-         },
-      }, { prefix = "<leader>" })
    end,
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -13,6 +13,64 @@ return {
         border = "shadow",
         winblend = 15,
     }
-  }
+  },
+
+      -- Which-Key reigster for Telescope group
+   config = function()
+      local wk = require("which-key")
+      wk.register({
+         f = {
+            name = "Find(Telescope)", -- optional group name
+            f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+            r = { "<cmd>Telescope oldfiles<cr>", "Find Recents", noremap = true}, -- additional options for creating the keymap
+            b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
+            g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+            h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+            j = { "<cmd>Telescope jumplist<cr>", "Jumplist" },
+            z = { "<cmd>Telescope colorscheme<cr>", "Temp Colorscheme" },
+            c = { "<cmd>Telescope grep_string<cr>", "Find Current Word" },
+
+         },
+      }, { prefix = "<leader>" })
+
+
+
+
+      -- Which-Key reigster for Git group
+      wk.register({
+         g = {
+            name = "Git",
+            g = {"LazyGit" },
+            n = { "<cmd>Neogit<CR>", "NeoGit" },
+            cc = {"Comment"},
+
+
+
+         }
+      })
+
+
+         -- { "<Leader>gn", ":Neogit<CR>" },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   end,
 }
 
