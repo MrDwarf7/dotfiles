3d6a817b5b667801f0b985b32336858e79b1f66d
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-12 18:40:18 +1000

    Bump.

---
+++ nvim/lua/vscode/init.lua
@@ -0,0 +1,39 @@
+return {
+    -- local maps = vim.keymap.set
+
+    -- Original as a .vim file... Hmmmmmmmmmmmmmm
+    -- " Better Navigation
+    -- nnoremap <silent> <C-j> :call VSCodeNotify('workbench.action.navigateDown')<CR>
+    -- xnoremap <silent> <C-j> :call VSCodeNotify('workbench.action.navigateDown')<CR>
+    -- nnoremap <silent> <C-k> :call VSCodeNotify('workbench.action.navigateUp')<CR>
+    -- xnoremap <silent> <C-k> :call VSCodeNotify('workbench.action.navigateUp')<CR>
+    -- nnoremap <silent> <C-h> :call VSCodeNotify('workbench.action.navigateLeft')<CR>
+    -- xnoremap <silent> <C-h> :call VSCodeNotify('workbench.action.navigateLeft')<CR>
+    -- nnoremap <silent> <C-l> :call VSCodeNotify('workbench.action.navigateRight')<CR>
+    -- xnoremap <silent> <C-l> :call VSCodeNotify('workbench.action.navigateRight')<CR>
+
+
+
+
+    -- require("vscode-neovim").notify({
+    -- -- " Better Navigation
+    -- -------- No idea what this one does tbh
+    -- -- nnoremap <silent> <C-w>_ :<C-u>call VSCodeNotify('workbench.action.toggleEditorWidths')<CR>
+
+    -- -- Better navigation
+    -- vim.keymap("n, v", "<C-j>", [[<cmd>call VSCodeNotify('workbench.action.navigateDown')<cr>]]),
+    -- vim.keymap.set("n, v", "<C-k>", [[<cmd>call VSCodeNotify('workbench.action.navigateUp')<cr>]]),
+
+    -- vim.keymap.set("n, h", "<C-k>", [[<cmd>call VSCodeNotify('workbench.action.navigateLeft')<cr>]]),
+    -- vim.keymap.set("n, v", "<C-l>", [[<cmd>call VSCodeNotify('workbench.action.navigateRight')<cr>]]),
+
+    -- -- Whichkey showtabline
+    -- vim.keymap.set("n, v", "<Space>", [[<cmd>call VSCodeNotify('whichkey.show')<cr>]]),
+
+    -- -- Optional settings
+    -- vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]]),
+    -- vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]]),
+    -- vim.keymap.set("i", "jj", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]]),
+    -- })
+
+}
