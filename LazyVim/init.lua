7bd3d9c92a4471f87e578a972a7e78acc91245d1
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-22 23:29:56 +1000

    Bump.

--- LazyVim/init.lua
+++ LazyVim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+    require("config.lazy_vscode")
+else
+    require("config.lazy")
+end
--- LazyVim/lua/config/lazy.lua
+++ LazyVim/lua/config/lazy.lua
@@ -2,18 +2,16 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 local full_spec = {
 
-  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-
+  { "LazyVim/LazyVim",                            import = "lazyvim.plugins" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.vscode" },
 
-  --
   -- add LazyVim and import its plugins
   -- { import = "lazyvim.plugins.extras.coding.copiot" },
   -- { import = "lazyvim.plugins.extras.linintg.eslint" },
