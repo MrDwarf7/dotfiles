3e548c9feefccd7eee68ab68a6b22d061a460bea
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-24 13:52:04 +1000

    Bump.

--- install.sh
+++ install.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
-
 create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
+	# Get the directory in which this script lives.
+	script_dir=$(dirname "$(readlink -f "$0")")
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+	# Get a list of all files in this directory that start with a dot.
+	files=$(find -maxdepth 1 -type f -name ".*")
 
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
+	# Create a symbolic link to each file in the home directory.
+	for file in $files; do
+		name=$(basename $file)
+		echo "Creating symlink to $name in home directory."
+		rm -rf ~/$name
+		ln -s $script_dir/$name ~/$name
+	done
 }
 
 create_symlinks
 
 echo cloning plugin repos
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$XDG_CONFIG_HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$XDG_CONFIG_HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# echo "Installing fonts."
+# FONT_DIR="$HOME/.fonts"
+# git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
+# cd $FONT_DIR
+# ./install.sh
 
-echo "Installing fonts."
-FONT_DIR="$HOME/.fonts"
-git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
-cd $FONT_DIR
-./install.sh
-
-
-echo Theming - 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-
+echo Theming -
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$XDG_CONFIG_HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 RESTART_ZSH="cd $HOME/ && exec .zsh"
 
