cdc142812ae7dbff1ef5f2ebb1d45cb3ef3c4454
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-09-30 13:44:18 +1000

    Bump.

--- LazyVim/lua/plugins/dap-tooling.lua
+++ LazyVim/lua/plugins/dap-tooling.lua
@@ -1,49 +1,49 @@
 return {
 
-    -- PYTHON DAP
+  -- PYTHON DAP
 
-    {
-        "nvim-neotest/neotest",
-        optional = true,
-        dependencies = {
-            "nvim-neotest/neotest-python",
-        },
-        opts = {
-            adapters = {
-                ["neotest-python"] = {
-                    -- Here you can specify the settings for the adapter, i.e.
-                    -- runner = "pytest",
-                    -- python = ".venv/bin/python",
-                },
-            },
-        },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
     },
-
-    {
-        "nvim-neotest/neotest-python",
-    },
-
-    {
-        "mfussenegger/nvim-dap",
-        optional = true,
-        dependencies = {
-            "mfussenegger/nvim-dap-python",
-            -- stylua: ignore
-            keys = {
-                { "<leader>d",   desc = "[D]ebugging" },
-                { "<leader>dp",  desc = "[P]ython" },
-                { "<leader>dpf", function() require('dap-python').test_method() end, desc = "[F]unction/Method Debug" },
-                { "<leader>dpc", function() require('dap-python').test_class() end,  desc = "[C]lass Debug" },
-            },
-            config = function()
-                local path = require("mason-registry").get_package("debugpy"):get_install_path()
-                require("dap-python").setup(path .. "/venv/bin/python")
-            end,
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
         },
+      },
     },
+  },
 
-    {
-        "mfussenegger/nvim-dap-python",
+  {
+    "nvim-neotest/neotest-python",
+  },
+
+  -- {
+  --     "mfussenegger/nvim-dap",
+  --     optional = true,
+  --     dependencies = {
+  --         "mfussenegger/nvim-dap-python",
+  --         -- stylua: ignore
+  --         keys = {
+  --             { "<leader>d",   desc = "[D]ebugging" },
+  --             { "<leader>dp",  desc = "[P]ython" },
+  --             { "<leader>dpf", function() require('dap-python').test_method() end, desc = "[F]unction/Method Debug" },
+  --             { "<leader>dpc", function() require('dap-python').test_class() end,  desc = "[C]lass Debug" },
+  --         },
+  --         config = function()
+  --             local path = require("mason-registry").get_package("debugpy"):get_install_path()
+  --             require("dap-python").setup(path .. "/venv/bin/python")
+  --         end,
+  --     },
+  -- },
+
+  {
+    "mfussenegger/nvim-dap-python",
         -- stylua: ignore
         keys = {
             { "<leader>d",   desc = "[D]ebugging" },
@@ -51,25 +51,25 @@ return {
             { "<leader>dpf", function() require('dap-python').test_method() end, desc = "[F]unction/Method Debug" },
             { "<leader>dpc", function() require('dap-python').test_class() end,  desc = "[C]lass Debug" },
         },
-        config = function()
-            local path = require("mason-registry").get_package("debugpy"):get_install_path()
-            require("dap-python").setup(path .. "/venv/bin/python")
-        end,
-    },
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      require("dap-python").setup(path .. "/venv/bin/python")
+    end,
+  },
 
-    {
-        "linux-cultist/venv-selector.nvim",
-        cmd = "VenvSelect",
-        opts = {
-            name = {
-                "venv",
-                ".venv",
-                "env",
-                ".env",
-            },
-        },
-        keys = { { "<leader>fv", "<cmd>:VenvSelect<cr>", desc = "Find [V]env" } },
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = {
+      name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+      },
     },
-    -- END PYTHON DAP
-    --
+    keys = { { "<leader>fv", "<cmd>:VenvSelect<cr>", desc = "Find [V]env" } },
+  },
+  -- END PYTHON DAP
+  --
 }
