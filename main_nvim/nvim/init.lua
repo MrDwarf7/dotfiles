cd0c4df05f4d7ee997ff1c1efb71481089659908
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-10-02 22:56:45 +1100

    Bump.

--- main_nvim/nvim/init.lua
+++ main_nvim/nvim/init.lua
@@ -1,9 +1,17 @@
-require('config.settings')
-require('config.color')
--- Lazy
-require('config.lazy')
---[[ require('config.font') ]]
-require('config.completion')
--- Keymap
-require('config.mappings')
-
+---@diagnostic disable: different-requires
+if not vim.g.vscode then
+    require('config.settings')
+    require('config.color')
+    -- Lazy
+    require('config.lazy')
+    require('config.completion')
+    -- Keymap
+    require('config.mappings')
+    --
+else -- If vscode
+    require('config.settings')
+    require('config.color')
+    require('config.lazy')
+    require('config.completion')
+    require('config.mappings')
+end
