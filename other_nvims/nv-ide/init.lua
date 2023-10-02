e4ace94151e3a197358a67e0a45b2549bac49728
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-10-02 21:47:38 +1100

    Deleted folder structure update

--- other_nvims/nv-ide/init.lua
+++
@@ -1,9 +0,0 @@
-require('config.settings')
-require('config.color')
--- Lazy
-require('config.lazy')
---[[ require('config.font') ]]
-require('config.completion')
--- Keymap
-require('config.mappings')
-
