3856e39831ea0dd5b026094d1a820d65e9992d96
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-10-05 18:48:41 +1100

    Removed ruby related items and plugins as I'm not a ruby/rails dev.

--- main_nvim/nvim/lua/config/plugins.lua
+++ main_nvim/nvim/lua/config/plugins.lua
@@ -320,8 +320,8 @@ return {
       vim.g.gutentags_project_root = { ".git" }
       vim.g.gutentags_ctags_extra_args = { "--fields=+l" }
       vim.g.gutentags_add_default_project_roots = 0
-      vim.g.gutentags_ctags_executable_ruby = "ripper-tags"
-      vim.g.gutentags_ctags_extra_args_ruby = { "--ignore-unsupported-options", "--recursive" }
+      --[[ vim.g.gutentags_ctags_executable_ruby = "ripper-tags" ]]
+      --[[ vim.g.gutentags_ctags_extra_args_ruby = { "--ignore-unsupported-options", "--recursive" } ]]
       -- vim.g.gutentags_trace = 1
     end,
   },
@@ -694,13 +694,10 @@ return {
 		},
 		},
 
-  {
-    "weizheheng/ror.nvim",
-  },
+  --[[ { ]]
+  --[[   "weizheheng/ror.nvim", ]]
+  --[[ }, ]]
 
 	{ 'echasnovski/mini.nvim', version = false },
-
-
-
 
 }
--- main_nvim/nvim/lua/config/plugins/nvim-dap.lua
+++ main_nvim/nvim/lua/config/plugins/nvim-dap.lua
@@ -38,22 +38,18 @@ function M.config()
       processId = require'dap.utils'.pick_process,
     },
   }
-  dap.adapters.ruby = {
-    type = 'executable';
-    command = 'bundle';
-    args = {'exec', 'readapt', 'stdio'};
-  }
 
-  dap.configurations.ruby = {
-    {
-      type = 'ruby';
-      request = 'launch';
-      name = 'Rails';
-      program = 'bundle';
-      programArgs = {'exec', 'rails', 's'};
-      useBundler = true;
-    },
-  }
+
+  --[[ dap.configurations.ruby = { ]]
+  --[[   { ]]
+  --[[     type = 'ruby'; ]]
+  --[[     request = 'launch'; ]]
+  --[[     name = 'Rails'; ]]
+  --[[     program = 'bundle'; ]]
+  --[[     programArgs = {'exec', 'rails', 's'}; ]]
+  --[[     useBundler = true; ]]
+  --[[   }, ]]
+  --[[ } ]]
 
 	dap.adapters.python = {
     type = 'executable',
