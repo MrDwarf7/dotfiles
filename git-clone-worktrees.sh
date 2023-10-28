3337469ee637f292b368f4117c1cdd5b6732a09a
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-10-28 12:25:00 +1100

    Addition of the git clone worktrees shell script to the windows ver.

---
+++ git-clone-worktrees.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+
+# Examples of call:
+# git-clone-bare-for-worktrees git@github.com:name/repo.git
+# => Clones to a /repo directory
+#
+# git-clone-bare-for-worktrees git@github.com:name/repo.git my-repo
+# => Clones to a /my-repo directory
+
+url=$1
+basename=${url##*/}
+name=${2:-${basename%.*}}
+
+if [ -z "$url" ]; then
+    echo "Usage: git-clone-bare-for-worktrees.sh <url> [<name>]"
+    exit 1
+fi
+if [ -d "$name" ]; then
+    echo "Directory $name already exists"
+    exit 1
+fi
+if [ -z "$name" ]; then
+    echo "Name is required"
+    exit 1
+fi
+
+
+mkdir $name
+cd "$name"
+
+# Moves all the administrative git files (a.k.a $GIT_DIR) under .bare directory.
+#
+# Plan is to create worktrees as siblings of this directory.
+# Example targeted structure:
+# .bare
+# main
+# new-awesome-feature
+# hotfix-bug-12
+# ...
+git clone --bare "$url" .bare
+echo "gitdir: ./.bare" > .git
+
+# Explicitly sets the remote origin fetch so we can fetch remote branches
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+# Gets all branches from origin
+git fetch origin
