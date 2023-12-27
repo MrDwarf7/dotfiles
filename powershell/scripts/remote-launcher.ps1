1a6bb040cb56d7f09ac38c8e2351eb04317a9939
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-12-27 16:57:10 +1100

    Addition of a remote launcher that can be source and have args passed into it to call remote programs a little easier.

---
+++ powershell/scripts/remote-launcher.ps1
@@ -0,0 +1,29 @@
+#
+
+function RemoteProcessLauncher
+{
+    param(
+        [string]$ProcessFolder,
+        [string]$ProcessExecutable,
+        $additionalArgs = $args
+    )
+
+    if ([String]::IsNullOrEmpty($ProcessFolder))
+    {
+        Write-Host "ProcessFolder is null or empty, defaulting."
+        $ProcessFolder = "C:\WINDOWS\system32"
+    }
+
+    if ([String]::IsNullOrEmpty($ProcessExecutable))
+    {
+        Write-Host "ProcessExecutable is null or empty, defaulting."
+        $ProcessExecutable = "notepad.exe"
+    }
+
+    $ProcessPath = "$ProcessFolder\$ProcessExecutable"
+    # Write-Host "ProcessPath: $ProcessPath"
+
+    Start-Process -FilePath $ProcessPath -ArgumentList $additionalArgs -PassThru
+
+}
+
