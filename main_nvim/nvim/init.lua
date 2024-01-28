063ed9785ade44b0cee3c1eb3cdc5865df5a9478
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-01-28 17:45:48 +1100

    chore: formatting for single to doubles

--- main_nvim/nvim/init.lua
+++ main_nvim/nvim/init.lua
@@ -1,11 +1,11 @@
 if vim.g.neovide then
-	require('config.neovide')
+	require("config.neovide")
 end
 
-require('config.settings')
-require('config.color')
+require("config.settings")
+require("config.color")
 -- Lazy
-require('config.lazy')
-require('config.completion')
+require("config.lazy")
+require("config.completion")
 -- Key map
-require('config.mappings')
+require("config.mappings")
