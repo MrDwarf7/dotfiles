c38d881562eff1625f1cad4aa1b788984e998598
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-01-30 14:06:38 +1100

    fix: dap working cross platform

--- .config/nvim/lua/configs/dap.lua
+++ .config/nvim/lua/configs/dap.lua
@@ -11,15 +11,53 @@ return {
 		local dap = require("dap")
 
 		vim.keymap.set("n", "<leader>d", "+[d]ebug", { desc = "+[d]ebug" })
-
 		vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "[c]ontinue" })
 		vim.keymap.set("n", "<Leader>.", dap.continue, { desc = "[c]ontinue" })
 		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "[b]reakpoint" })
 		vim.keymap.set("n", "<Leader>dC", dap.terminate, { desc = "[C]ancel" })
 
+		local architechture = function()
+			local os_type = function()
+				if vim.fn.has("win32") == 1 then
+					return "win32"
+				end
+				if vim.fn.has("unix") == 1 then
+					return "unix"
+				end
+			end
+			return os_type()
+		end
+
+		local debugpy_adapater = function()
+			local arch = architechture()
+			if arch == "win32" then
+				local os_call = os.getenv("USERPROFILE") .. "/scoop/apps/python/current/python.exe"
+				return os_call
+			end
+			if arch == "unix" then
+				local os_call_lin = os.getenv("HOME") .. "/.config/.pyenv/versions/3.12.1/bin/python3.12"
+				return os_call_lin
+			end
+		end
+
+		local path = function()
+			local arch = architechture()
+			local path_set
+
+			if arch == "win32" then
+				local win_venv = vim.fn.getcwd() .. "/.venv/Scripts/python.exe"
+				path_set = win_venv
+			end
+			if arch == "unix" then
+				local lin_venv = vim.fn.getcwd() .. "/.venv/bin/python"
+				path_set = lin_venv
+			end
+			return path_set
+		end
+
 		dap.adapters.python = {
 			type = "executable",
-			command = os.getenv("HOME") .. "/.config/.pyenv/versions/3.12.1/bin/python3.12",
+			command = debugpy_adapater(),
 			args = { "-m", "debugpy.adapter" },
 		}
 
@@ -30,8 +68,7 @@ return {
 				name = "Launch file",
 				program = "${file}",
 				pythonPath = function()
-					local cwd = vim.fn.getcwd()
-					local python_path = vim.fn.glob(cwd .. "/.venv/bin/python")
+					local python_path = path()
 					return python_path
 				end,
 			},
@@ -86,10 +123,6 @@ return {
 			dapui.close()
 		end
 
-		-- vim.keymap.set("n", "<Leader>dt", function()
-		-- 	dapui.toggle()
-		-- end, { desc = "[t]oggle" })
-
 		vim.keymap.set("n", "<Leader>dw", function()
 			require("dapui").toggle()
 		end, { desc = "[w]indow" })
@@ -103,19 +136,23 @@ return {
 		end, { desc = "dap CLOSE" })
 
 		vim.keymap.set("n", "<Leader>di", function()
-			require("dapui").StepInto()
+			require("dapui").step_into()
 		end, { desc = "[i]nto" })
 
 		vim.keymap.set("n", "<Leader>do", function()
-			require("dapui").StepOver()
+			require("dapui").step_over()
 		end, { desc = "[o]ver" })
 
 		vim.keymap.set("n", "<Leader>dO", function()
-			require("dapui").StepOut()
+			require("dapui").step_out()
 		end, { desc = "[O]ut" })
 
 		vim.keymap.set({ "n", "v" }, "<A-k>", function()
 			require("dapui").eval()
 		end)
+
+		vim.keymap.set("n", "<Leader>dz", function()
+			require("dapui").open({ reset = true })
+		end, { desc = "[z]eval" })
 	end,
 }
