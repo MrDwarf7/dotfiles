68c18c0d04895198d1331be9bccde7ece044b3ba
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-01 01:24:14 +1100

    chore: sync to linux branch

--- .config/nvim/lua/configs/git.lua
+++ .config/nvim/lua/configs/git.lua
@@ -6,7 +6,13 @@ return {
 			"nvim-telescope/telescope.nvim",
 			"nvim-lua/plenary.nvim",
 		},
-		config = function()
+
+		keys = {
+			{ "n", "<leader>gg", "<cmd>LazyGit<CR>",                                           { desc = "[l]azy Git" } },
+			{ "n", "<Leader>gp", ":lua require('telescope').extensions.lazygit.lazygit()<CR>", { desc = "[p]rojects" } }
+		},
+
+		config = function(_, opts)
 			require("telescope")
 			require("telescope").load_extension("lazygit")
 			local autocmd = vim.api.nvim_create_autocmd
@@ -36,7 +42,7 @@ return {
 
 	{
 		"lewis6991/gitsigns.nvim",
-		event = "ModeChanged",
+		lazy = false,
 		opts = {
 			signs = {
 				add = { text = "│" },
@@ -47,5 +53,69 @@ return {
 				untracked = { text = "│" },
 			},
 		},
+		config = function(_, opts)
+			require("gitsigns").setup(opts)
+			-- function diffThisBranch()
+			-- 	local branch = vim.fn.input("Branch: ", "")
+			-- 	require "gitsigns".diffthis(branch)
+			-- end
+
+			-- Navigation
+			-- vim.keymap.set('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+			-- vim.keymap.set('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+			vim.keymap.set('n', ']c', '<cmd>Gitsigns next_hunk<CR>')
+			vim.keymap.set('n', '[c', '<cmd>Gitsigns prev_hunk<CR>')
+
+			-- Actions
+			vim.keymap.set({ 'n', 'v' }, '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+			-- vim.keymap.set('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+			vim.keymap.set({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+			-- vim.keymap.set('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+			vim.keymap.set('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+			vim.keymap.set('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+			vim.keymap.set('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+			vim.keymap.set('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+			vim.keymap.set('n', '<leader>hb', function()
+				require "gitsigns".blame_line { full = true }
+			end)
+			vim.keymap.set('n', '<leader>ht',
+				'<cmd>Gitsigns toggle_current_line_blame<CR>')
+			vim.keymap.set('n', '<leader>hT', '<cmd>Gitsigns toggle_deleted<CR>')
+
+			vim.keymap.set('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+			vim.keymap.set('n', '<leader>hD',
+				function() require "gitsigns".diffthis("~") end)
+			vim.keymap.set('n', '<leader>hm',
+				function() require "gitsigns".diffthis("main") end)
+
+			-- vim.keymap.set('n', '<leader>hM', diffThisBranch)
+
+			-- Text object
+			vim.keymap.set('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+			vim.keymap.set('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+		end
 	},
+
+
+	{
+		'sindrets/diffview.nvim',
+		event = "BufEnter",
+		keys = {
+			vim.keymap.set('n', '<Leader>gd', '+[d]iffview', {}),
+			vim.keymap.set('n', '<Leader>gdo', '<cmd>DiffviewOpen<CR>', {}),
+			vim.keymap.set('n', '<Leader>gdc', '<cmd>DiffviewClose<CR>', {}),
+			vim.keymap.set('n', '<Leader>gdf', '<cmd>DiffviewToggleFiles<CR>', {}),
+			vim.keymap.set('n', '<Leader>gdr', '<cmd>DiffviewRefresh<CR>', {}),
+		},
+		config = function(_, opts)
+			require('diffview').setup(_, opts)
+		end
+
+	},
+
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+	},
+
 }
