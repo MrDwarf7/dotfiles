70b0b6d0a03384c11e7f082abbf68e2a367e8d67
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-03 02:29:51 +1100

    feat/depr: add second/old last_pos autocmd

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -13,6 +13,20 @@ autocmd("BufReadPost", {
 	end,
 })
 
+-- go to last loc when opening a buffer
+-- autocmd('BufReadPost', {
+--     callback = function()
+--         local mark = vim.api.nvim_buf_get_mark(0, '"')
+--         local lcount = vim.api.nvim_buf_line_count(0)
+--         if mark[1] > 0 and mark[1] <= lcount then
+--             pcall(vim.api.nvim_win_set_cursor, 0, mark)
+--         end
+--     end,
+-- })
+
+
+
+
 autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank({ timeout = 60 })
