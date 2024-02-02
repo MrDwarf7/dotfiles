3bb4bf0dfea6df612b5e850c2e7e89e8a5300189
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-02 21:44:58 +1100

    feat: add v-mode indenting keymap

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -25,6 +25,9 @@ vim.keymap.set("v", "p", '"_dP', silent_opts)
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", silent_opts) -- Shifting lines down / move
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", silent_opts) -- Shifting lines up / move
 
+vim.keymap.set("v", "<", "<gv", silent_opts)
+vim.keymap.set("v", ">", ">gv", silent_opts)
+
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
