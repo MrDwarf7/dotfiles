66213927df6b73861e2fcdd7c13e58c0370e0f0b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-03 16:23:23 +1100

    feat: change undotree bind from <Leader>U -> U

--- .config/nvim/after/plugin/undotree.lua
+++ .config/nvim/after/plugin/undotree.lua
@@ -2,4 +2,4 @@ vim.g.undotree_SetFocusWhenToggle = 1
 vim.g.undotree_HighlightChangedWithSign = 1
 vim.g.undotree_WindowLayout = 1
 
-vim.keymap.set("n", "<Leader>U", ":UndotreeToggle<CR>", { silent = true, desc = "[U]ndo-tree" })
+vim.keymap.set("n", "U", ":UndotreeToggle<CR>", { silent = true, desc = "[U]ndo-tree" })
