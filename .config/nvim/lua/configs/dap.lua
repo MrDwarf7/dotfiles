cb7c938180a6566c5af021c964857b002bd03f11
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-04 02:33:13 +1100

    chore: remove dapui collapse on finish

--- .config/nvim/lua/configs/dap.lua
+++ .config/nvim/lua/configs/dap.lua
@@ -106,12 +106,14 @@ return {
 		dap.listeners.before.launch.dapui_config = function()
 			dapui.open()
 		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+
+		-- dap.listeners.before.event_terminated.dapui_config = function()
+		-- 	dapui.close()
+		-- end
+
+		-- dap.listeners.before.event_exited.dapui_config = function()
+		-- 	dapui.close()
+		-- end
 
 		vim.keymap.set("n", "<Leader>dw", function()
 			require("dapui").toggle()
