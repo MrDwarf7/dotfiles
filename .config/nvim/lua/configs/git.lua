7c02aca6b59590c1e47eb9ad395acf15b57d9a7f
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-06 10:26:41 +1100

    feat: add default settings/setup for gitsigns
    Default line blame = true,
    virt text handling and line blame opts,
    remainer default settings added to file.

--- .config/nvim/lua/configs/git.lua
+++ .config/nvim/lua/configs/git.lua
@@ -43,6 +43,20 @@ return {
 					changedelete = { text = "~" },
 					untracked = { text = "│" },
 				},
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' (default) | 'overlay' | 'right_align'
+					delay = 750,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+				},
+				current_line_blame_formatter = "<author> - <summary> - <author_time:%Y-%m-%d>",
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+
 				on_attach = function()
 					local gs = package.loaded.gitsigns
 
