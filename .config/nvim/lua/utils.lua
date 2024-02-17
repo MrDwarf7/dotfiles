6c4e151fddf09e58c07a8a3776e6cbcfc4527e84
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-18 00:35:27 +1100

    chore: formatting

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,4 +1,5 @@
 local M = {}
+local utils = require('lspconfig.util')
 
 M.lazy_load = function(plugin)
 	vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile" }, {
@@ -28,10 +29,40 @@ M.lazy_load = function(plugin)
 	})
 end
 
-M.on_attach = function(_, bufnr)
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
+-- M.on_attach = function(_, bufnr)
+-- 	local function buf_set_option(...)
+-- 		vim.api.nvim_buf_set_option(bufnr, ...)
+-- 	end
+-- end
+
+
+M.make_cmd = function(new_config)
+	local temp_path = vim.fn.stdpath 'cache'
+	if new_config.bundle_path ~= nil then
+		local command_fmt =
+		[[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
+		local command = command_fmt:format(new_config.bundle_path, new_config.bundle_path, temp_path, temp_path)
+		return { new_config.shell, '-NoLogo', '-NoProfile', '-Command', command }
 	end
 end
+
+M.powershell_es = function()
+	return {
+		default_config = {
+			shell = 'pwsh',
+			on_new_config = function(new_config, _)
+				-- Don't overwrite cmd if already set
+				if not new_config.cmd then
+					new_config.cmd = M.make_cmd(new_config)
+				end
+			end,
+
+			filetypes = { 'ps1' },
+			root_dir = utils.find_git_ancestor,
+			single_file_support = true,
+		}
+	}
+end
+
 
 return M
