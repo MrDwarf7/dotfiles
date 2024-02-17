0f0a94e40e3ad3edfd93a3d80c8ecd71a0c77713
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-18 00:33:44 +1100

    style: formatting mostly

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -2,16 +2,19 @@
 
 return {
 	"nvim-lualine/lualine.nvim",
-	event = "CursorMoved",
+	lazy = false,
+
+	--event = "CursorMoved",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		local my_theme = {
-			-- evil_line = require("lualine_confs.evil_line").setup()
-			-- bubbles = require("lualine_confs.bubbles").setup(),
-			tokyonight = "tokyonight",
-		}
+		local my_theme = "tokyonight"
+		-- {
+		-- evil_line = require("lualine_confs.evil_line").setup()
+		-- bubbles = require("lualine_confs.bubbles").setup(),
+		-- tokyonight = "tokyonight",
+		-- }
 		require("lualine").setup({
 			theme = my_theme,
 		})
