57bd4215e81120d502ff8a868aca35def79e6f37
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-02-18 14:31:27 +1100

    fix: remove filetype.nvim plugin

--- .config/nvim/lua/configs/init.lua
+++ .config/nvim/lua/configs/init.lua
@@ -1,26 +1,5 @@
 return {
 	{
-		"nathom/filetype.nvim",
-		lazy = false,
-		priority = 900,
-		init = function()
-			require("filetype").setup({
-				overrides = {
-					extensions = {
-						sh = "bash",
-						c = "c",
-					},
-					literal = {
-						["Dockerfile"] = "docker",
-						["docker-compose"] = "docker",
-						[".zshrc"] = "zsh",
-					},
-				},
-			})
-		end,
-	},
-
-	{
 		"prichrd/netrw.nvim",
 		lazy = false,
 		priority = 998,
