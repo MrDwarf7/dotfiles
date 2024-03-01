3d725160ac443b6464378379f5c99d59c88ebb05
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-03-01 22:06:58 +1100

    style: formatting

--- .config/NewVim/lua/configs/lsp_related/mason-lspconfig.lua
+++ .config/NewVim/lua/configs/lsp_related/mason-lspconfig.lua
@@ -8,92 +8,92 @@ local nvim_lsp_servers_list = require("configs.lsp_related.nvim-lsp-servers")
 local ensure_installed_table = {}
 
 if vim.g.os == "unix" then
-	ensure_installed_table = {
-		--"awk_ls", -- Un-updated (requires running LTS version of node via NVM)
-		"azure_pipelines_ls",
-		"bashls",
-		"biome",
-		"clangd",
-		"cmake",
-		"cssls",
-		"docker_compose_language_service",
-		"dockerls",
-		"eslint",
-		"gopls",
-		"html",
-		"lua_ls",
-		"marksman",
-		-- "powershell_es",
-		"prismals",
-		"pyright",
-		"ruff_lsp",
-		-- "rust_analyzer", -- Since using Rustaceanvim, DO NOT SETUP via lspconfig call
-		"slint_lsp",
-		"tailwindcss",
-		"taplo",
-		-- "tsserver",
-		"vimls",
-		"yamlls",
-		"zls",
-	}
+    ensure_installed_table = {
+        --"awk_ls", -- Un-updated (requires running LTS version of node via NVM)
+        "azure_pipelines_ls",
+        "bashls",
+        "biome",
+        "clangd",
+        "cmake",
+        "cssls",
+        "docker_compose_language_service",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "html",
+        "lua_ls",
+        "marksman",
+        -- "powershell_es",
+        "prismals",
+        "pyright",
+        "ruff_lsp",
+        -- "rust_analyzer", -- Since using Rustaceanvim, DO NOT SETUP via lspconfig call
+        "slint_lsp",
+        "tailwindcss",
+        "taplo",
+        -- "tsserver",
+        "vimls",
+        "yamlls",
+        "zls",
+    }
 elseif vim.g.os == "win32" then
-	ensure_installed_table = {
-		--"awk_ls", -- Un-updated (requires running LTS version of node via NVM)
-		"azure_pipelines_ls",
-		-- "bashls",
-		"biome",
-		"clangd",
-		"cmake",
-		"cssls",
-		"docker_compose_language_service",
-		"dockerls",
-		"eslint",
-		"gopls",
-		"html",
-		"lua_ls",
-		"marksman",
-		"powershell_es",
-		"prismals",
-		"pyright",
-		"ruff_lsp",
-		-- "rust_analyzer", -- Since using Rustaceanvim, DO NOT SETUP via lspconfig call
-		"slint_lsp",
-		"tailwindcss",
-		"taplo",
-		-- "tsserver",
-		"vimls",
-		"yamlls",
-		"zls",
-	}
+    ensure_installed_table = {
+        --"awk_ls", -- Un-updated (requires running LTS version of node via NVM)
+        "azure_pipelines_ls",
+        -- "bashls",
+        "biome",
+        "clangd",
+        "cmake",
+        "cssls",
+        "docker_compose_language_service",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "html",
+        "lua_ls",
+        "marksman",
+        "powershell_es",
+        "prismals",
+        "pyright",
+        "ruff_lsp",
+        -- "rust_analyzer", -- Since using Rustaceanvim, DO NOT SETUP via lspconfig call
+        "slint_lsp",
+        "tailwindcss",
+        "taplo",
+        -- "tsserver",
+        "vimls",
+        "yamlls",
+        "zls",
+    }
 end
 
 mason_lspconfig.setup({
-	ensure_installed = ensure_installed_table,
-	automatic_installation = {
-		exclude = { "rust_analyzer", "pyright", "clangd", "ruff_lsp", "tsserver" },
-	},
+    ensure_installed = ensure_installed_table,
+    automatic_installation = {
+        exclude = { "rust_analyzer", "pyright", "clangd", "ruff_lsp", "tsserver" },
+    },
 
-	handlers = {
-		function(server_name)
-			if server_name == "clangd" then
-				return
-			end
-			if server_name == "rust_analyzer" then
-				return
-			end
-			-- mason_lspconfig.clangd = function()
-			--     nvim_lsp_servers_list.clangd.setup()
-			--     nvim_lsp_config_file.lsp_on_attach()
-			--     capabilities = nvim_lsp_servers_list[nvim_lsp.clangd.setup(capabilities)]
-			-- end
+    handlers = {
+        function(server_name)
+            if server_name == "clangd" then
+                return
+            end
+            if server_name == "rust_analyzer" then
+                return
+            end
+            -- mason_lspconfig.clangd = function()
+            --     nvim_lsp_servers_list.clangd.setup()
+            --     nvim_lsp_config_file.lsp_on_attach()
+            --     capabilities = nvim_lsp_servers_list[nvim_lsp.clangd.setup(capabilities)]
+            -- end
 
-			nvim_lsp[server_name].setup({
-				on_attach = function()
-					nvim_lsp_config_file.lsp_on_attach()
-					-- inlay_hints_attach()
-				end,
-				capabilities = capabilities.default_capabilities(),
-			})
-		end,
-	},
+            nvim_lsp[server_name].setup({
+                on_attach = function()
+                    nvim_lsp_config_file.lsp_on_attach()
+                    -- inlay_hints_attach()
+                end,
+                capabilities = capabilities.default_capabilities(),
+            })
+        end,
+    },
 })
