bd1d41495148df8e15a6b7875e16a049d0b3a4b8
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-03-12 18:41:41 +1100

    chore: add work specific config(lazys)

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,42 @@
 # Installed via pip install typer-cli
 
 $currentPSModulePath = $env:PSModulePath
+$workDefaultPSModulePath = "C:\Applications\PowerShell_start\Modules"
 $env:PSModulePath=[NullString]
+$env:PYTHON_PATH=[NullString]
+# Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+$LazyLoadProfile = [PowerShell]::Create()
+[void]$LazyLoadProfile.AddScript(@'
+    Import-Module DockerCompletion
+    Import-Module PSReadLine
+    Import-Module -Name CompletionPredictor
+    Import-Module C:\Applications\PowerShell_start\Modules\posh-git\1.1.0\posh-git.psm1
+'@)
+
+
+$LazyLoadProfileRunspace = [RunspaceFactory]::CreateRunspace()
+$LazyLoadProfile.Runspace = $LazyLoadProfileRunspace
+$LazyLoadProfileRunspace.Open()
+[void]$LazyLoadProfile.BeginInvoke()
+
+$null = Register-ObjectEvent -InputObject $LazyLoadProfile -EventName InvocationStateChanged -Action {
+    Import-Module PSReadLine
+    Import-Module posh-git
+    Import-Module -Name DockerCompletion
+    Import-Module PSReadLine
+    Import-Module -Name CompletionPredictor
+    # Import-Module "$HOME\scoop\apps\posh-git\1.1.0\posh-git.psm1"
+    Import-Module C:\Applications\PowerShell_start\Modules\posh-git\1.1.0\posh-git.psm1
+    $global:GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
+    $global:GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
+    $global:GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+    $global:GitPromptSettings.DefaultPromptAfterSuffix.Text = ''
+    #
+    $LazyLoadProfile.Dispose()
+    $LazyLoadProfileRunspace.Close()
+    $LazyLoadProfileRunspace.Dispose()
+}
 
 # Dotfiles copy
 $env:HOME_PROFILE = $false
@@ -43,8 +78,6 @@ $powershell_scripts_dir = "$powershell_dir\scripts"
 
 $env:EDITOR = $env:VISUAL = 'nvim'
 
-
-
 # $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 # if (Test-Path($ChocolateyProfile))
 # {
@@ -58,7 +91,6 @@ function Test-CommandExists ([Parameter(Mandatory = $true)][string] $Command)
 {
     return [bool](Get-Command $Command -ErrorAction SilentlyContinue)
 }
-
 
 # Work.sort of
 function checkEnvironment
@@ -79,8 +111,9 @@ function checkEnvironment
 # END - Tooling Functions
 
 # Work
-if (checkEnvironment)
+if (checkEnvironment -eq $true)
 {
+    $env:PSModulePath = $workDefaultPSModulePath
     . "$powershell_scripts_dir\work_scripts.ps1"
 }
 
@@ -107,8 +140,6 @@ if (-not (checkEnvironment))
 #     . $PROFILE
 # }
 
-
-
 function .
 {
     Start-Process .
@@ -124,6 +155,7 @@ function l
 {
     param ($path = ".")
     Get-ChildItem $path -Force
+    # [System.IO.Directory]::GetFiles($path) -Force
 }
 
 # if I decide to start using starship, well this is how I would do it.
