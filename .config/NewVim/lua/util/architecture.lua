22be5155c42a758468f1e0dbe1dc50216b977c0a
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-03-27 11:00:01 +1100

    feat: architecture call code

--- .config/NewVim/lua/util/architecture.lua
+++ .config/NewVim/lua/util/architecture.lua
@@ -1,15 +1,12 @@
 local M = {}
 
-M.architechture = function()
-    local os_type = function()
-        if vim.fn.has("win32") == 1 then
-            return "win32"
-        end
-        if vim.fn.has("unix") == 1 then
-            return "unix"
-        end
-    end
-    return os_type()
+M.is_windows = function()
+	-- local os_type = function()
+	if vim.fn.has("win32") == 1 then
+		return true
+	else
+		return false
+	end
 end
 
 return M
