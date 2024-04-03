7b54a79048acf8d023d6baa60b035c31f907e61d
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-04-04 02:47:40 +1100

    style: formatting

--- .config/NewVim/lua/vscode_conf/mappings.lua
+++ .config/NewVim/lua/vscode_conf/mappings.lua
@@ -9,6 +9,8 @@ local silent_opts = { noremap = true, silent = true }
 g.mapleader = " "
 g.maplocalleader = " "
 
+map("n", "<Leader>e", vim.cmd.Ex, { desc = "[e]xplorer" })
+
 map("n", "<Esc>", ":nohl<CR>", silent_opts)
 map("v", "<Esc>", "<Esc>:nohl<CR>", silent_opts)
 map("i", "jj", "<Esc>", silent_opts)
@@ -51,6 +53,9 @@ map("n", "y<S-l>", "y$", silent_opts) -- Same as above for yanking
 map("n", "d<S-h>", "d^", silent_opts) -- Same as above for yanking
 map("n", "d<S-l>", "d$", silent_opts) -- Same as above for yanking
 
+map("n", "<Leader>pl", ":Lazy<CR>", silent_opts, { desc = "[l]azy" })
+map("n", "<Leader>pm", ":Mason<CR>", silent_opts, { desc = "[m]ason" })
+
 map("n", "dd", function() -- Empty/blank lines go into blackhole register
 	if #current_line() == 0 then
 		return '"_dd'
@@ -67,10 +72,16 @@ map("n", "<C-l>", ":vertical resize -2<CR>", silent_opts)
 map("n", "<Down>", ":resize -2<CR>", silent_opts)
 map("n", "<Up>", ":resize +2<CR>", silent_opts)
 
+map("n", "<C-w>e", "<C-w>=", silent_opts, { desc = "[e]qualize" }) -- ctrl + w + = : easier to hit to equalize the width of buffers
 map("n", "<C-w>X", "<cmd>only<CR>", silent_opts, { desc = "buffers - CLOSE all except" })
 
+map("n", "]b", "<cmd>bnext<cr>", { desc = "[n]ext" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "[p]revious" })
 
 map("n", "<Leader>bN", "<cmd>enew<cr>", { desc = "New File" })
+
+map("n", "<Leader>bn", ":bnext<CR>", silent_opts, { desc = "[n]ext" })
+map("n", "<Leader>bp", ":bprev<CR>", silent_opts, { desc = "[p]revious" })
 
 map("n", "<Leader>x", ":bdelete<CR>", silent_opts, { desc = "[X]close" })
 map("n", "<Leader>?", ":vsplit<CR>:terminal<CR>A", silent_opts, { desc = "Inbuilt Term" }) -- Temp for the time being until lazygit // fugitive or something
