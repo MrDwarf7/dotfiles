81ec201321e2bd4f55ebfada87b3bf89efd1efc6
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-04-04 02:34:12 +1100

    feat/chore: add file_ignore_patterns to telescope + keymaps for open in splits

--- .config/NewVim/lua/configs/telescope.lua
+++ .config/NewVim/lua/configs/telescope.lua
@@ -52,7 +52,7 @@ require("telescope").setup({
 		},
 		border = {},
 		color_devicons = true,
-		file_ignore_patterns = { "node_modules", ".venv", "venv" },
+		file_ignore_patterns = { "node_modules", ".venv", "venv", "deps", "incremental", "build" },
 		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
 		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
 		prompt_prefix = "   ",
@@ -70,6 +70,10 @@ require("telescope").setup({
 				["<C-t>"] = trouble_prov.open_with_trouble,
 				["<C-q>"] = actions.close,
 				["<C-d>"] = actions.delete_buffer,
+
+				["<C-s>"] = actions.select_horizontal,
+				["<C-l>"] = actions.select_vertical,
+
 			},
 			n = {
 				["q"] = actions.close,
@@ -80,6 +84,9 @@ require("telescope").setup({
 				["<C-p>"] = actions.move_selection_previous,
 				["<C-n>"] = actions.move_selection_next,
 				["<C-d>"] = actions.delete_buffer,
+
+				["<C-s>"] = actions.select_horizontal,
+				["<C-l>"] = actions.select_vertical,
 			},
 		},
 	},
