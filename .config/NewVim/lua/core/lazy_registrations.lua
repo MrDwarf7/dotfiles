d8f8ed59db47a580991947096c188266945375b9
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-04-04 02:56:39 +1100

    refactor: cmp deps revised with removal of non-required packages/plugins

--- .config/NewVim/lua/core/lazy_registrations.lua
+++ .config/NewVim/lua/core/lazy_registrations.lua
@@ -449,23 +449,19 @@ return {
 		event = "InsertEnter",
 		dependencies = {
 			"neovim/nvim-lspconfig",
-			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+
+			"zbirenbaum/copilot-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"petertriho/cmp-git",
-			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
 
-			"L3MON4D3/LuaSnip",
-
 			"saadparwaiz1/cmp_luasnip",
-			"zbirenbaum/copilot-cmp",
-			-- "zbirenbaum/copilot.lua",
-
 			"saecki/crates.nvim",
 			"vrslev/cmp-pypi",
-
 			"onsails/lspkind.nvim",
 		},
 		config = function()
