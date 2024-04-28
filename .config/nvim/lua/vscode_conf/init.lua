4741e2100ee76dc8b516e342addf2abbb736e140
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-04-28 22:21:28 +1000

    refactor: move vscode_conf cursor fixes to sep. file

--- .config/nvim/lua/vscode_conf/init.lua
+++ .config/nvim/lua/vscode_conf/init.lua
@@ -17,24 +17,8 @@ set clipboard+=unnamedplus
 	vim.g.mapleader = " "
 	vim.g.maplocalleader = " "
 
-	local function moveCursor(direction)
-		if vim.v.count == 0 and (vim.fn.reg_recording() == "" and vim.fn.reg_executing() == "") then
-			return ("g" .. direction)
-		else
-			return direction
-		end
-	end
-
-	vim.keymap.set("n", "j", function()
-		return moveCursor("j")
-	end, { expr = true, remap = true })
-
-	vim.keymap.set("n", "k", function()
-		return moveCursor("k")
-	end, { expr = true, remap = true })
-	--
-
 	print("Vscode specific setup file loads...")
+	require("vscode_conf.move_cursor")
 	require("vscode_conf.actions")
 	require("vscode_conf.options")
 	require("vscode_conf.mappings")
