11c4bf92ef1f609c7673c2ca31025e8e8aeb8d3a
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-05-31 20:02:47 +1000

    per: vscode module loading prio(s)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,74 +22,76 @@ if vim.g.neovide then
 		require("core.options"),
 		require("core.mappings"),
 	}
-else
-	if vim.g.vscode then
-		print("Welcome to VSCode Neovim...")
-		require("vscode-neovim")
-		print("Local vscode required -> ")
-		return require("vscode_conf").setup()
-	end
 end
 
-return {
-	require("core.options"),
-	require("core.mappings"),
-	require("core.autocmds"),
+if vim.g.vscode then
+	print("Welcome to VSCode Neovim...")
+	require("vscode-neovim")
+	print("Local vscode required -> ")
+	return require("vscode_conf").setup()
+end
 
-	require("lazy").setup("configs", {
-		defaults = { lazy = true },
-		change_detection = {
-			notify = false,
-		},
+if vim.g.neovide == nil and vim.g.vscode == nil then
+	return {
+		require("core.options"),
+		require("core.mappings"),
+		require("core.autocmds"),
 
-		performance = {
-
-			cache = {
-				-- enabled = true,
-				path = vim.fn.stdpath("cache") .. "/lazy",
-
-				-- disable_events = { "VimEnter", "BufReadPre" },
-				ttl = 3600 * 24 * 7,
+		require("lazy").setup("configs", {
+			defaults = { lazy = true },
+			change_detection = {
+				notify = false,
 			},
 
-			-- reset_packagepath = true,
-			rtp = {
-				reset = true,
-				disabled_plugins = {
-					"2html_plugin",
-					"bugreport",
-					"compiler",
-					"ftplugin",
-					"getscript",
-					"getscriptPlugin",
-					"gzip",
-					"logipat",
-					"matchit",
-					-- "netrw",
-					"netrwFileHandlers",
-					-- "netrwPlugin",
-					"netrwSettings",
-					"optwin",
-					"rplugin",
-					"rrhelper",
-					"spellfile_plugin",
-					"synmenu",
-					"syntax",
-					"tar",
-					"tarPlugin",
-					"tohtml",
-					"tutor",
-					"vimball",
-					"vimballPlugin",
-					"zip",
-					"zipPlugin",
+			performance = {
+
+				cache = {
+					-- enabled = true,
+					path = vim.fn.stdpath("cache") .. "/lazy",
+
+					-- disable_events = { "VimEnter", "BufReadPre" },
+					ttl = 3600 * 24 * 7,
+				},
+
+				-- reset_packagepath = true,
+				rtp = {
+					reset = true,
+					disabled_plugins = {
+						"2html_plugin",
+						"bugreport",
+						"compiler",
+						"ftplugin",
+						"getscript",
+						"getscriptPlugin",
+						"gzip",
+						"logipat",
+						"matchit",
+						-- "netrw",
+						"netrwFileHandlers",
+						-- "netrwPlugin",
+						"netrwSettings",
+						"optwin",
+						"rplugin",
+						"rrhelper",
+						"spellfile_plugin",
+						"synmenu",
+						"syntax",
+						"tar",
+						"tarPlugin",
+						"tohtml",
+						"tutor",
+						"vimball",
+						"vimballPlugin",
+						"zip",
+						"zipPlugin",
+					},
 				},
 			},
-		},
 
-		ui = {
-			size = { width = 0.7, height = 0.7 },
-			border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
-		},
-	}),
-}
+			ui = {
+				size = { width = 0.7, height = 0.7 },
+				border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+			},
+		}),
+	}
+end
