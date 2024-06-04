35a1447927280ac1f8a9f41263e4cec015600103
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-06-04 23:16:40 +1000

    chore: remove dead code + update branch for venv-selector

--- .config/nvim/lua/configs/venv-selector.lua
+++ .config/nvim/lua/configs/venv-selector.lua
@@ -1,36 +1,22 @@
--- 	config = function()
--- 		require("configs.venv-selector")
--- 	end,
--- },
---
-
 return {
 	"linux-cultist/venv-selector.nvim",
+	branch = "regexp",
 	lazy = true,
 	event = "InsertEnter",
-	dependencies = {
-		-- "neovim/nvim-lspconfig",
-		-- "nvim-telescope/telescope.nvim",
-		-- "mfussenegger/nvim-dap-python",
-	},
 	config = function()
-		local autocmd = vim.api.nvim_create_autocmd
-		local map = vim.keymap.set
-		local fn = vim.fn
-		local opts = { silent = true, nowait = true }
-
 		require("venv-selector").setup({
 			name = { ".venv", ".venv/", "$HOME/.config/.pyenv/versions/3.12.1/bin/python3" },
 			pdm_path = "pdm",
 		})
 
-		autocmd("BufReadPost", {
+		local opts = { silent = true, nowait = true }
+		vim.api.nvim_create_autocmd("BufReadPost", {
 			desc = "Auto select virtualenv Nvim open",
 			pattern = "*",
 			callback = function()
-				map("n", "<Leader>fv", "<cmd>VenvSelect<CR>", opts)
-				map("n", "<Leader>fz", "<cmd>VenvSelectCached<CR>", opts)
-				local venv = fn.findfile("pyproject.toml", fn.getcwd() .. ".venv")
+				vim.keymap.set("n", "<Leader>fv", "<cmd>VenvSelect<CR>", opts)
+				vim.keymap.set("n", "<Leader>fz", "<cmd>VenvSelectCached<CR>", opts)
+				local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ".venv")
 				if venv ~= "" then
 					require("venv-selector").retrieve_from_cache()
 				end
