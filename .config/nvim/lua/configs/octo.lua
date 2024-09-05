f77da1b526447d1ac693bd69a21dc3f6f6154d01
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-05 23:43:16 +1000

    feat(nvim/github): Add cool github tooling to nvim

--- .config/nvim/lua/configs/octo.lua
+++ .config/nvim/lua/configs/octo.lua
@@ -9,13 +9,20 @@ return {
 	},
 	keys = {
 		{
-			"<Leader>gho",
-			":Octo ",
+			"<Leader>O",
+			"<cmd>Octo<CR>",
+			desc = "Octo",
+		},
+
+		{
+			"<Leader>gO",
+			"<cmd>Octo<CR>",
 			desc = "Octo",
 		},
 	},
 
 	opts = {
+		enable_builtin = true,
 		suppress_missing_scope = {
 			projects_v2 = true,
 		},
