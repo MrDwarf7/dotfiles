2ddbca8218ecd0ee84321f75ccee9473416c3684
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-11 17:36:29 +1000

    test(nvim_dbee): test out nvim_dbee

--- .config/nvim/lua/configs/database.lua
+++ .config/nvim/lua/configs/database.lua
@@ -96,4 +96,23 @@ return {
 			})
 		end,
 	},
+
+	------------------ testing
+
+	-- {
+	-- 	"kndndrj/nvim-dbee",
+	-- 	lazy = false,
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 	},
+	-- 	build = function()
+	-- 		-- Install tries to automatically detect the install method.
+	-- 		-- if it fails, try calling it with one of these parameters:
+	-- 		--    "curl", "wget", "bitsadmin", "go"
+	-- 		require("dbee").install("go")
+	-- 	end,
+	-- 	config = function()
+	-- 		require("dbee").setup(--[[optional config]])
+	-- 	end,
+	-- },
 }
