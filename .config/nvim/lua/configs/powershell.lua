a6f8c87b3aa91fdac5747b8a3d31adbd3efe4e7e
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-13 00:14:09 +1000

    partial(cmp-changes): commit 1/2, need restore also

--- .config/nvim/lua/configs/powershell.lua
+++ .config/nvim/lua/configs/powershell.lua
@@ -1,6 +1,7 @@
 return {
 	"TheLeoP/powershell.nvim",
-	lazy = false,
+	ft = { "ps1", "psm1", "psd1" },
+	-- lazy = false,
 	opts = {
 		bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
 		capabilities = vim.lsp.protocol.make_client_capabilities(),
