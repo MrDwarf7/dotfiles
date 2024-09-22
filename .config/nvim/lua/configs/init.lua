7f169aee7959e5070373b2ddedc62ba19d32948b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-22 18:29:37 +1000

    test(completed/revert): No difference - Revert

--- .config/nvim/lua/configs/init.lua
+++ .config/nvim/lua/configs/init.lua
@@ -9,19 +9,8 @@ return {
 			{ "<Leader>gF", "<cmd>Git fetch --all<CR>", mode = "n", desc = "fetch --all" },
 
 			{ "<Leader>gp", "<cmd>Git pull<CR>", mode = "n", desc = "pull" },
-			{
-				"<Leader>gP",
-				function()
-					vim.schedule(function()
-						vim.cmd([[ Git push ]])
-					end)
-				end,
-				mode = "n",
-				desc = "push",
-			},
-			-- "<cmd>Git push<CR>", mode = "n", desc = "push" },
-			--
-			--
+			{ "<Leader>gP", "<cmd>Git push<CR>", mode = "n", desc = "push" },
+
 			{ "<Leader>gl", "<cmd>Gclog<CR>", mode = "n", desc = "log [quickfix]" },
 			{ "<Leader>gL", "<cmd>Gllog<CR>", mode = "n", desc = "log [location]" },
 
