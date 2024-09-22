44e71e8259acbcba153e73c907342752c5b69ea3
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-22 18:26:24 +1000

    refactor(nvim): Change database file to use returns

--- .config/nvim/lua/configs/database.lua
+++ .config/nvim/lua/configs/database.lua
@@ -82,22 +82,59 @@ return {
 			})
 		end,
 
-	------------------ testing
+		-- Now we just hand them straight out/to the plugin
 
-	-- {
-	-- 	"kndndrj/nvim-dbee",
-	-- 	lazy = false,
-	-- 	dependencies = {
-	-- 		"MunifTanjim/nui.nvim",
-	-- 	},
-	-- 	build = function()
-	-- 		-- Install tries to automatically detect the install method.
-	-- 		-- if it fails, try calling it with one of these parameters:
-	-- 		--    "curl", "wget", "bitsadmin", "go"
-	-- 		require("dbee").install("go")
-	-- 	end,
-	-- 	config = function()
-	-- 		require("dbee").setup(--[[optional config]])
-	-- 	end,
-	-- },
+		config = function(_, opts)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"sql",
+				},
+				command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+			})
+			return opts
+		end,
+	},
 }
+
+-- opts
+-- Your DBUI configuration
+-- vim.g.db_ui_show_help = 0 -- Disable help block at top left
+-- vim.g.db_ui_use_nerd_fonts = 1
+-- vim.g.db_ui_auto_execute_table_helpers = 1
+-- vim.g.db_ui_winwidth = 35
+--
+-- vim.g.db_ui_save_location = vim.fn.stdpath("cache") .. "/db_ui_save_location"
+-- local opts = {
+-- 	db_completion = function()
+-- 		return require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+-- 	end,
+-- }
+-- config = function(_, opts)
+-- 	-- Your DBUI configuration
+-- 	vim.g.db_ui_use_nerd_fonts = 1
+-- 	vim.g.db_ui_auto_execute_table_helpers = 1
+-- 	-- vim.g.db_ui_show_help = 0 -- Disable help block at top left
+-- 	vim.g.db_ui_winwidth = 35
+--
+-- 	-- vim.g.db_ui_save_location = vim.fn.stdpath("cache") .. "/db_ui_save_location"
+--
+-- 	vim.api.nvim_create_autocmd("FileType", {
+-- 		pattern = {
+-- 			"sql",
+-- 		},
+-- 		command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+-- 	})
+--
+-- 	vim.api.nvim_create_autocmd("FileType", {
+-- 		pattern = {
+-- 			"sql",
+-- 			"mysql",
+-- 			"plsql",
+-- 			"mssql",
+-- 			"tsql",
+-- 		},
+-- 		callback = function()
+-- 			vim.schedule(opts.db_completion)
+-- 		end,
+-- 	})
+-- end,
