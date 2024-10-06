a707b173f5c52fa4d589487a8ba5ef95a454d9d4
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-10-06 18:40:38 +1100

    fix(typescript): Update notify & typescript_tools to work happily

--- .config/nvim/lua/configs/typescript_tools.lua
+++ .config/nvim/lua/configs/typescript_tools.lua
@@ -1,5 +1,6 @@
 return {
 	"pmizio/typescript-tools.nvim",
+	lazy = true,
 	ft = {
 		"css",
 		"html",
@@ -22,5 +23,35 @@ return {
 		"nvim-lua/plenary.nvim",
 		"neovim/nvim-lspconfig",
 	},
-	opts = {},
+	opts = {
+		settings = {
+			tsserver_file_preferences = {
+				includeCompletionsForImportStatements = true,
+				includeCompletionsWithSnippetText = true,
+				useLabelDetailsInCompletionEntries = true,
+
+				-- Params
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+
+				-- Functions
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				-- Variables
+				includeInlayVariableTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+				-- Properties
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+
+				quotePreference = "double",
+			},
+			-- tsserver_plugins = {
+			-- }
+		},
+	},
+	config = function(_, opts)
+		opts = opts or {}
+		require("typescript-tools").setup(opts)
+	end,
 }
