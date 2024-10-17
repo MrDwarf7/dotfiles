7e93d345591154e35e79ae6c0768939a1a8b2511
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-10-17 17:53:19 +1100

    bug(remote_nvim): Tentative working config settings

--- .config/nvim/lua/configs/remote_nvim.lua
+++ .config/nvim/lua/configs/remote_nvim.lua
@@ -1,18 +1,57 @@
+local utils = require("remote-nvim.utils")
+
 local ssh_folder = function()
-	if vim.fn.has("win32") == 1 then
-		return vim.fn.expand("$USERPROFILE") .. "/.ssh"
-	else
-		return vim.fn.expand("$HOME") .. "/.ssh"
+	if vim.g.os == "Windows_NT" then
+		local path = vim.fn.expand("$USERPROFILE") .. "\\.ssh"
+		-- Fix slashs
+		-- path = string.gsub(path, "\\", "/")
+		return path
+		-- else
+		-- 	return vim.fn.expand("$HOME") .. "/.ssh"
 	end
 end
 
 return {
 	"amitds1997/remote-nvim.nvim",
+	lazy = false,
 	version = "*", -- Pin to GitHub releases
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- For standard functions
 		"MunifTanjim/nui.nvim", -- To build the plugin UI
 		"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
 	},
-	config = true,
+	opts = {
+		ssh_config = {
+
+			-- ssh_config = true,
+			ssh_config_file_paths = { utils.path_join(true, ssh_folder(), "config") },
+		},
+		-- offline_mode = {
+		-- 	enabled = true,
+		-- },
+	},
+	config = function(o, opts)
+		local rem = require("remote-nvim")
+		-- Testing if I can force via deep/or access certain nested(s)
+
+		opts = vim.tbl_deep_extend("force", o, opts)
+
+		rem.setup({
+			host = "Windows",
+			neovim_user_config_path = vim.fn.stdpath("config"),
+			ssh_config = {
+				ssh_config_file_paths = { utils.path_join(true, ssh_folder(), "config") },
+				ssh_binary = "ssh",
+				scp_binary = "scp",
+			},
+		})
+		-- rem.setup({
+		-- 	neovim_user_config_path = vim.fn.stdpath("config"),
+		-- ssh_config = {
+		--
+		-- 	-- ssh_config = true,
+		-- 	ssh_config_file_paths = { utils.path_join(true, ssh_folder(), "config") },
+		-- },
+		-- })
+	end,
 }
