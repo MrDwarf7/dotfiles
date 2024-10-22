4337217f4954d551a2bd389b1a8778ba80af9884
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-10-22 21:13:26 +1100

    feat/wip: gitui vim binds & custom plugin?!??

---
+++ .config/nvim/lua/configs/gitrizz.lua
@@ -0,0 +1,13 @@
+local dev_dir = require("util.maybe").maybe_dev("gitrizz")
+
+-- return {
+-- 	"MrDwarf7/gitrizz.nvim",
+-- 	dev = dev_dir,
+-- 	lazy = false,
+--
+-- 	-- keys = {}
+--
+-- 	opts = {},
+-- }
+
+return {}
