9503ce0a2d44951f97fee09985f89290070061a7
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-10-26 02:44:06 +1100

    feat(yazi)!: Officially working & configured

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,6 @@
 require("git"):setup()
 
-require("relative-motions"):setup()
+require("relative-motions"):setup({ only_motions = true })
 
 -- THEME.git_modified = ui.Style():fg("blue")
 -- THEME.git_deleted = ui.Style():fg("red"):bold()
