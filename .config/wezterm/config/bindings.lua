0a960d22e65f33e8c323f7dffef44b5536f58371
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-11-05 17:50:32 +1100

    chore(wezterm/binds): Modify tab//pane binds

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -45,10 +45,6 @@ local key_opts = {
 	{ key = "-", mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 
 	-- Panes
-	{ key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
 
 	{ key = "d", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
 
@@ -58,10 +54,16 @@ local key_opts = {
 	{ key = "UpArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{ key = "RightArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Right", 5 }) },
 
+	-- Panes still
+	{ key = "h", mods = "LEADER|CTRL", action = act({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER|CTRL", action = act({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER|CTRL", action = act({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER|CTRL", action = act({ ActivatePaneDirection = "Right" }) },
+
 	-- Tabs
 	-- Was either this or q and e, mapping w to close instead of q
-	{ key = "h", mods = "LEADER|CTRL", action = act.ActivateTabRelative(-1) },
-	{ key = "l", mods = "LEADER|CTRL", action = act.ActivateTabRelative(1) },
+	{ key = "h", mods = "LEADER", action = act.ActivateTabRelative(-1) }, -- Moving
+	{ key = "l", mods = "LEADER", action = act.ActivateTabRelative(1) }, -- Moving
 
 	{ key = "L", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 	{ key = "H", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
@@ -85,7 +87,7 @@ local key_opts = {
 
 	{
 		key = "w",
-		mods = "LEADER|CTRL",
+		mods = "LEADER",
 		action = act.PaneSelect({ alphabet = "123456789", mode = "SwapWithActiveKeepFocus" }),
 	},
 
