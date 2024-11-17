9ca37b8484a52aa7494f4205a7dbbdd36c95b2c8
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-11-17 15:41:08 +1100

    reg(prompt): Create copy of base prompt for regr. reasons

--- .config/fish/_fish_prompt.fish
+++ .config/fish/_fish_prompt.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
-
+#
+# Original fish prompt + minor tweak
 function fish_prompt --description 'Informative prompt'
     #Save the return status of the previous command
     set -l last_pipestatus $pipestatus
@@ -15,7 +16,7 @@ function fish_prompt --description 'Informative prompt'
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
+        printf '%s%s@%s %s%s %s%s%s \n> ' (set_color brblue) \
             $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
             (set_color normal)
     end
