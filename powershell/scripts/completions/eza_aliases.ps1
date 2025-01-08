66a8adfb6e9141b6a98002f8bb7f77028cc28ead
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-01-09 03:47:53 +1100

    MODIFY!: eza arguments changed

--- powershell/scripts/completions/eza_aliases.ps1
+++ powershell/scripts/completions/eza_aliases.ps1
@@ -15,7 +15,7 @@ function ClearAndList {
         $ag = $args
     )
     Clear-Host
-    $eza_args = "-a --tree --level=1 --icons=always --follow-symlinks";
+    $eza_args = "-lah --color=always --follow-symlinks --icons=always --git";
     & RunEza -Path $ag -CliArgs $eza_args;
 }
 
@@ -25,27 +25,27 @@ function EasyList {
     param (
         $ag = $args
     )
-    $eza_args = "-alh --color=always --level=1 --follow-symlinks";
+    $eza_args = "-lah --color=always --follow-symlinks --icons=always";
     & RunEza -Path $ag -CliArgs $eza_args;
 }
 
-New-Alias -Name l -Value EasyList -Force;
+New-Alias -Name la -Value EasyList -Force;
 
 function AltList {
     param (
         $ag = $args
     )
-    $eza_args = "-a --tree --level=1 --icons=always --follow-symlinks";
+    $eza_args = "-a --color=always --follow-symlinks --icons=always --git --tree --level=1";
     & RunEza -Path $ag -CliArgs $eza_args;
 }
 
-New-Alias -Name la -Value AltList -Force
+New-Alias -Name l -Value AltList -Force
 
 function ListTree {
     param (
         $ag = $args
     )
-    $eza_args = "-a --tree --level=3 --icons=always --follow-symlinks";
+    $eza_args = "-a --color=always --follow-symlinks --icons=always --git --tree --level=3";
     & RunEza -Path $ag -CliArgs $eza_args;
 }
 
