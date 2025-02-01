6284e3fd3dde88516fd8203181673f23c23aa21a
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-02-01 19:48:46 +1100

    fix/feat: add keytables + H/L binds for CopyMode

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -1,8 +1,10 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local utils = require("utils.utils_init")
 -- local platform = require("utils.platform")
 
 local keys = {}
+local key_tables = {}
 
 -- if platform is x then
 -- 	keys.leader = thing
@@ -15,6 +17,12 @@ local launch_menu = { flags = "LAUNCH_MENU_ITEMS" }
 
 local tab_keys = {}
 for i = 1, 8 do
+	-- local t = utils.tbl_deep_extend("force", tab_keys, {
+	-- 	{ key = tostring(i), mods = "CTRL", action = act({ ActivateTab = i - 1 }), },
+	-- 	{ key = tostring(i), mods = "LEADER", action = act({ ActivateTab = i - 1 }), },
+	-- })
+	-- tab_keys = t
+
 	-- ctrl + a -> number to activate that tab
 	table.insert(tab_keys, {
 		key = tostring(i),
@@ -39,7 +47,6 @@ local key_opts = {
 	{ key = "c", mods = "LEADER", action = act.SpawnTab("DefaultDomain") },
 	{ key = "C", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 	{ key = "m", mods = "LEADER", action = act.SpawnTab({ DomainName = "WSL:Arch" }) },
-	-- act({ SpawnTab = "CurrentPaneDomain" }) },
 
 	-- { key = "Nmod.SUPER", mods = "", action = act.SpawnWindow },
 	{ key = "F", mods = "LEADER", action = "ToggleFullScreen" },
@@ -53,8 +60,8 @@ local key_opts = {
 
 	-- Pane resizing
 	{ key = "LeftArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "DownArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Down", 5 }) },
-	{ key = "UpArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "DownArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Down", 4 }) },
+	{ key = "UpArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Up", 4 }) },
 	{ key = "RightArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Right", 5 }) },
 
 	-- Panes still
@@ -92,6 +99,9 @@ local key_opts = {
 
 	-- Modes
 	{ key = "y", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+	{ key = "v", mods = "LEADER", action = act.ActivateKeyTable({ name = "scrolling", one_shot = false }) },
+
 	{ key = "g", mods = "LEADER", action = act.QuickSelect }, -- 'Grab'
 	{ key = "f", mods = "LEADER", action = act.Search({ CaseSensitiveString = "" }) },
 	-- TODO: Bindings for opacity
@@ -137,11 +147,50 @@ local key_opts = {
 	table.unpack(tab_keys),
 }
 
-for _, key_op in ipairs(key_opts) do
-	table.insert(keys, key_op)
+utils.merge_tables(keys, key_opts)
+
+local key_table_opts = {
+	copy_mode = {
+		{ key = "H", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "H", mods = "SHIFT", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "L", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+		{ key = "L", mods = "SHIFT", action = act.CopyMode("MoveToEndOfLineContent") },
+
+		{ key = "e", mods = "CTRL", action = act.CopyMode({ MoveByPage = 0.1 }) },
+		{ key = "y", mods = "CTRL", action = act.CopyMode({ MoveByPage = -0.1 }) },
+	},
+	resize_pane = {
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 2 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 2 }) },
+	},
+
+	scrolling = {
+		{ key = "Escape", action = "PopKeyTable" },
+
+		{ key = "e", mods = "CTRL", action = act.ScrollByLine(1) },
+		{ key = "y", mods = "CTRL", action = act.ScrollByLine(-1) },
+
+		{ key = "j", mods = "NONE", action = act.ScrollByLine(2) },
+		{ key = "k", mods = "NONE", action = act.ScrollByLine(-2) },
+
+		{ key = "j", mods = "CTRL", action = act.ScrollByLine(2) },
+		{ key = "k", mods = "CTRL", action = act.ScrollByLine(-2) },
+
+		{ key = "d", mods = "CTRL", action = act.ScrollByPage(1) },
+		{ key = "u", mods = "CTRL", action = act.ScrollByPage(-1) },
+	},
+}
+
+--- Merge the default key tables with the custom ones we have
+--- THis preserved the existing (default) binds
+for k, v in pairs(wezterm.gui.default_key_tables()) do
+	key_tables[k] = v
 end
 
-local key_tables = {}
+utils.merge_tables(key_tables, key_table_opts)
 
 local mouse_bindings = {
 	{
@@ -157,6 +206,14 @@ local mouse_bindings = {
 		action = act.StartWindowDrag,
 	},
 }
+
+wezterm.on("update-right-status", function(window, pane)
+	local name = window:active_key_table()
+	if name then
+		name = "T-MODE: " .. name .. " "
+	end
+	window:set_right_status(name or "")
+end)
 
 return {
 	leader = leader,
