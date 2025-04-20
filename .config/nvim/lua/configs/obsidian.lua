d4a6fd039e6ef7a4a972b21c971992ff958cd019
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-04-20 22:43:16 +1000

    chore: update vim + add obsidian support

---
+++ .config/nvim/lua/configs/obsidian.lua
@@ -0,0 +1,68 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	-- 	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 	-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	-- 	-- refer to `:h file-pattern` for more examples
+	-- 	"BufReadPre E:/Obsidian/StoneVault/*.md",
+	-- 	"BufNewFile E:/Obsidian/StoneVault/*.md",
+	-- },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "StoneVault",
+				path = "E:/Obsidian/StoneVault",
+			},
+		},
+		notes_subdir = "1. Daily Notes",
+		log_level = vim.log.levels.INFO, -- This IS exposed!
+
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+		-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+		-- way then set 'mappings = {}'.
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		new_notes_location = "notes_subdir",
+	},
+
+	config = function(opts)
+		opts = opts or {}
+		vim.opt.conceallevel = 2
+
+		return require("obsidian").setup(opts)
+	end,
+}
