e90ddf394df6dc8e5c2a9dc2ba7ebb88a1cf7ba5
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-05-22 07:01:15 +1000

    feat: add qutebrowser to .config

---
+++ .config/qutebrowser/config/binds.py
@@ -0,0 +1,117 @@
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+
+# pylint: disable=C0111
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
+
+def bind_chained(key, *commands):
+    config.bind(key, " ;; ".join(commands))
+
+
+def alias_chained(key, *commands):
+    c.aliases[key] = " ;; ".join(commands)
+
+
+# Aliases
+c.aliases["q"] = "tab-close"
+c.aliases["wa"] = "session-save"
+alias_chained("wq", "session-save", "tab-close")
+
+
+# config.bind("<Ctrl+e>", ":cmd-run-with-count 1 scroll down")
+# config.bind("<Ctrl+y>", ":cmd-run-with-count 1 scroll up")
+
+
+config.bind("<Ctrl+y>", "scroll-px 0 -25")
+config.bind("<Ctrl+e>", "scroll-px 0 25")
+
+config.bind("k", "scroll-px 0 -150")
+config.bind("j", "scroll-px 0 150")
+
+
+config.bind("t", "cmd-set-text -s :open -t")
+config.bind("T", "cmd-set-text -s :open -t !g ")
+
+config.bind("o", "cmd-set-text :open {url:pretty}")
+config.bind("O", "cmd-set-text :open -t -r {url:pretty}")
+
+
+config.bind("b", "cmd-set-text -sr :tab-focus")
+
+
+config.bind("'", "cmd-set-text -s :quickmark-load")
+config.bind('"', "cmd-set-text -s :quickmark-load -t")
+
+# keybinding changes
+# config.bind('=', 'cmd-set-text -s :open')
+# config.bind('h', 'history')
+# config.bind("cs", "cmd-set-text -s :config-source")
+config.bind("cs", "config-source")
+# config.bind('tH', 'config-cycle tabs.show multiple never')
+# config.bind('sH', 'config-cycle statusbar.show always never')
+# config.bind('T', 'hint links tab')
+# config.bind('pP', 'open -- {primary}')
+# config.bind('pp', 'open -- {clipboard}')
+# config.bind('pt', 'open -t -- {clipboard}')
+# config.bind('qm', 'macro-record')
+# config.bind('<ctrl-y>', 'spawn --userscript ytdl.sh')
+# config.bind('tT', 'config-cycle tabs.position top left')
+# config.bind('gJ', 'tab-move +')
+# config.bind('gK', 'tab-move -')
+# config.bind('gm', 'tab-move')
+
+
+#### Original binds
+# config.bind("tsh", "config-cycle -p -t -u *://{url:host}/* content.javascript.enabled ;; reload")
+# config.bind("tSh", "config-cycle -p -u *://{url:host}/* content.javascript.enabled ;; reload")
+# config.bind("tsH", "config-cycle -p -t -u *://*.{url:host}/* content.javascript.enabled ;; reload")
+# config.bind("tSH", "config-cycle -p -u *://*.{url:host}/* content.javascript.enabled ;; reload")
+# config.bind("tsu", "config-cycle -p -t -u {url} content.javascript.enabled ;; reload")
+# config.bind("tSu", "config-cycle -p -u {url} content.javascript.enabled ;; reload")
+# config.bind("tph", "config-cycle -p -t -u *://{url:host}/* content.plugins ;; reload")
+# config.bind("tPh", "config-cycle -p -u *://{url:host}/* content.plugins ;; reload")
+# config.bind("tpH", "config-cycle -p -t -u *://*.{url:host}/* content.plugins ;; reload")
+# config.bind("tPH", "config-cycle -p -u *://*.{url:host}/* content.plugins ;; reload")
+# config.bind("tpu", "config-cycle -p -t -u {url} content.plugins ;; reload")
+# config.bind("tPu", "config-cycle -p -u {url} content.plugins ;; reload")
+# config.bind("tih", "config-cycle -p -t -u *://{url:host}/* content.images ;; reload")
+# config.bind("tIh", "config-cycle -p -u *://{url:host}/* content.images ;; reload")
+# config.bind("tiH", "config-cycle -p -t -u *://*.{url:host}/* content.images ;; reload")
+# config.bind("tIH", "config-cycle -p -u *://*.{url:host}/* content.images ;; reload")
+# config.bind("tiu", "config-cycle -p -t -u {url} content.images ;; reload")
+# config.bind("tIu", "config-cycle -p -u {url} content.images ;; reload")
+# config.bind("tch", "config-cycle -p -t -u *://{url:host}/* content.cookies.accept all no-3rdparty never ;; reload")
+# config.bind("tCh", "config-cycle -p -u *://{url:host}/* content.cookies.accept all no-3rdparty never ;; reload")
+# config.bind("tcH", "config-cycle -p -t -u *://*.{url:host}/* content.cookies.accept all no-3rdparty never ;; reload")
+# config.bind("tCH", "config-cycle -p -u *://*.{url:host}/* content.cookies.accept all no-3rdparty never ;; reload")
+# config.bind("tcu", "config-cycle -p -t -u {url} content.cookies.accept all no-3rdparty never ;; reload")
+# config.bind("tCu", "config-cycle -p -u {url} content.cookies.accept all no-3rdparty never ;; reload")
+
+
+config.bind("zi", "zoom-in")
+config.bind("zo", "zoom-out")
+config.bind("zz", "zoom 100%")
+config.bind("zn", "clear-messages")
+
+
+config.bind(">", "tab-move +")
+config.bind("<", "tab-move -")
+
+
+config.bind("K", "tab-prev")
+config.bind("J", "tab-next")
+
+
+config.bind("<Ctrl+Shift+Tab>", "tab-prev")
+config.bind("<Ctrl+Tab>", "tab-next")
+
+
+config.bind("yi", "tab-clone")
+config.bind(":q", "tab-close")
+
+
+config.bind("<Ctrl+o>", "back")
+config.bind("<Ctrl+i>", "forward")
---
+++ .config/qutebrowser/config/config.py
@@ -0,0 +1,183 @@
+import subprocess
+import os
+from urllib.request import urlopen
+
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+
+# pylint: disable=C0111
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
+c.colors.statusbar.normal.bg = "#00000000"
+c.colors.statusbar.command.bg = "#00000000"
+
+# c.url.start_pages = ""
+# c.url.default_page = ""
+
+c.tabs.title.format = "{audio}{current_title}"
+c.fonts.web.size.default = 20
+
+# c.hints.mode = ""
+
+
+c.scrolling.smooth = False  # smooth scrolling -- NOTE smooth scrolling does not work with the :scroll-px command.
+# c.scroll_px = 100
+
+
+c.url.default_page = "https://www.google.com/"
+# c.url.default_page = "about:blank"
+
+c.url.searchengines = {
+    # note - if you use duckduckgo, you can make use of its built in bangs, of which there are many! https://duckduckgo.com/bangs
+    # "DEFAULT": "https://duckduckgo.com/?q={}",
+    "DEFAULT": "https://www.google.com/?q={}",
+    "!g": "https://www.google.com/search?q={}",
+    "!aw": "https://wiki.archlinux.org/?search={}",
+    "!apkg": "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=",
+    "!gh": "https://github.com/search?o=desc&q={}&s=stars",
+    "!yt": "https://www.youtube.com/results?search_query={}",
+    "!drs": "https://docs.rs/releases/search?query={}",
+    "!crates": "https://crates.io/search?q={}",
+}
+
+c.completion.shrink = True
+
+c.completion.open_categories = [
+    "searchengines",
+    "quickmarks",
+    "bookmarks",
+    "history",
+    "filesystem",
+]
+
+# config.load_autoconfig()
+
+c.auto_save.session = True
+
+
+if os.path.exists(config.configdir / "binds.py"):
+    config.source(str(config.configdir / "binds.py"))
+
+
+def add_to_sorted_lst(ext_list, new_vals):
+    return sorted(ext_list + new_vals)
+
+
+c.input.insert_mode.auto_load = False
+
+config.set("input.insert_mode.leave_on_load", True, "https://github.com")
+
+
+# additional_zoom_levels = [105, 115, 120, 130, 135, 140, 145]
+# new_list = add_to_sorted_lst(c.zoom.levels, additional_zoom_levels)
+# c.zoom.levels = new_list
+
+c.zoom.levels = [
+    25,
+    33,
+    50,
+    67,
+    75,
+    90,
+    100,
+    110,
+    120,
+    130,
+    140,
+    150,
+    160,
+    170,
+    180,
+    190,
+    200,
+    210,
+    220,
+    250,
+    300,
+    350,
+    400,
+    450,
+    500,
+]
+# Darkmode settings
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+c.colors.webpage.darkmode.policy.images = "never"
+config.set("colors.webpage.darkmode.enabled", False, "file://*")
+
+
+# Styles/cosmetic
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+
+    theme.setup(c, "macchiato", False)
+
+
+c.tabs.position = "left"
+c.tabs.padding = {"top": 4, "bottom": 4, "left": 9, "right": 9}
+c.tabs.indicator.width = 2  # tab indicators (little line thing)
+c.tabs.width = "7%"
+
+
+# Fonts
+c.fonts.default_family = []
+c.fonts.default_size = "11pt"
+c.fonts.web.family.fixed = "monospace"
+c.fonts.web.family.sans_serif = "monospace"
+c.fonts.web.family.serif = "monospace"
+c.fonts.web.family.standard = "monospace"
+
+
+# Privacy
+c.completion.cmd_history_max_items = 1
+# config.set("completion.cmd_history_max_items", 1)
+c.content.private_browsing = True
+# config.set("content.private_browsing", True)
+
+config.set("content.webgl", False, "*")
+c.content.canvas_reading = False
+# config.set("content.canvas_reading", False)
+c.content.geolocation = False
+# config.set("content.geolocation", False)
+c.content.webrtc_ip_handling_policy = "default-public-interface-only"
+# config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
+c.content.cookies.accept = "all"
+# config.set("content.cookies.accept", "all")
+c.content.cookies.store = True
+# config.set("content.cookies.store", True)
+c.content.javascript.enabled = True
+# config.set("content.javascript.enabled", True)  #### Setup keybinds
+
+c.content.blocking.enabled = True
+c.content.blocking.method = "adblock"
+
+c.content.blocking.adblock.lists = [
+    # "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2025.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+]
