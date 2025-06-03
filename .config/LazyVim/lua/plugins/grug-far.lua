af997e141f07a459d3cbeb55e9a622e8488e37a5
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-06-03 19:19:39 +1000

    feat(LazyVim): configure grug-far keys

---
+++ .config/LazyVim/lua/plugins/grug-far.lua
@@ -0,0 +1,32 @@
+local invoke = function()
+  local grug = require("grug-far")
+  local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+  grug.open({
+    transient = true,
+    prefills = {
+      filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+    },
+  })
+end
+
+return {
+  "grug-far.nvim",
+  keys = {
+    {
+      "<Leader>lR",
+      function()
+        invoke()
+      end,
+      mode = { "n", "v" },
+      desc = "Search & Replace",
+    },
+    {
+      "<Leader>fs",
+      function()
+        invoke()
+      end,
+      mode = { "n", "v" },
+      desc = "Search & Replace",
+    },
+  },
+}
