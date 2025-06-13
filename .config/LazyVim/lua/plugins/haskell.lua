2d0d19d345b14f204957abbc52f590fe267e70d1
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-06-13 22:53:56 +1000

    chore(LazyVim): add haskell-tools

---
+++ .config/LazyVim/lua/plugins/haskell.lua
@@ -0,0 +1,18 @@
+-- local ht = require("haskell-tools")
+-- local bufnr = vim.api.nvim_get_current_buf()
+-- local opts = { noremap = true, silent = true, buffer = bufnr }
+--
+-- local map = function(keys, fn, opts)
+--   vim.keymap.set("n", keys, fn, opts)
+-- end
+
+return {
+  "haskell-tools.nvim",
+  version = "^5",
+  lazy = false,
+  ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  dependencies = {
+    { "nvim-telescope/telescope.nvim", optional = true },
+  },
+  -- opts = {}
+}
