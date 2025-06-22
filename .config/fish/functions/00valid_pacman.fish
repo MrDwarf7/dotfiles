a73b9d19f645ed0f4ed03e3202d3329cd81059e2
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-06-22 18:00:01 +1000

    fix/feat: have 00valid handle quotes over caller

--- .config/fish/functions/00valid_pacman.fish
+++ .config/fish/functions/00valid_pacman.fish
@@ -8,14 +8,23 @@
 # Returns:
 # 0 if the package is installed 
 # 1 if the package is not installed
-function 00valid_pacman 
+function 00valid_pacman
     set -l to_test_prog $argv[1]
 
-    if command pacman -Qi $to_test_prog &> /dev/null
+    # Check for both types of quotes and remove them if present
+    # Allows the caller to be ambiguous with quotes (with/without)
+    if string match -q '"*"' -- $to_test_prog
+        set to_test_prog (string replace -r '^"|"$' '' -- $to_test_prog)
+    end
+
+    if string match -q "'*'" -- $to_test_prog
+        set to_test_prog (string replace -r "^'|'" -- $to_test_prog)
+    end
+
+    if command pacman -Qi "$to_test_prog" &>/dev/null
         return 0
     else
         return 1
     end
     return 1
 end
-
