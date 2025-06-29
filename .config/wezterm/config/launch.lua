7ca61d70d26c6adc0824e492dff9f531955314da
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-06-29 15:07:05 +1000

    style(wezterm): cleanup launch.lua

--- .config/wezterm/config/launch.lua
+++ .config/wezterm/config/launch.lua
@@ -1,23 +1,29 @@
 local platform = require("utils.platform")
 local utils = require("utils.utils_init")
 
--- working commit
--- 6a8ac7f5
+local function is_win()
+	if utils.env("SHELL") ~= nil then
+		return utils.env("SHELL")
+	end
+	if utils.env("SHELL") == nil then
+		return "pwsh"
+	end
+end
+
+local function is_linux()
+	if utils.env("SHELL") ~= nil then
+		return utils.env("SHELL")
+	else
+		return { "fish" }
+	end
+end
+
 local function shell()
 	if platform.is_win then
-		if utils.env("SHELL") ~= nil then
-			return utils.env("SHELL")
-		end
-		if utils.env("SHELL") == nil then
-			return "pwsh"
-		end
+		return is_win()
 		-- return utils.env("SHELL") or "C:\\Program Files\\PowerShell\\7-preview\\pwsh.exe"
 	elseif platform.is_linux or platform.is_mac then
-		if utils.env("SHELL") ~= nil then
-			return utils.env("SHELL")
-		else
-			return { "fish" }
-		end
+		return is_linux()
 	end
 end
 -- default_prog = { shell(), "-NoLogo" }
@@ -29,6 +35,7 @@ local options = {
 
 -- Order matters, place more wanted at the top of the table.insert stack
 
+-- Nushell is universal across both Windows & Linux
 table.insert(options.launch_menu, { label = "Nushell", args = { "nu" } })
 
 if platform.is_win then
@@ -36,32 +43,7 @@ if platform.is_win then
 	table.insert(options.launch_menu, { label = "Pwsh", args = { utils.env("SHELL"), "-NoLogo" } })
 	table.insert(options.launch_menu, { label = "Pwsh -NoProfile", args = { utils.env("SHELL"), "-NoProfile" } })
 	table.insert(options.launch_menu, { label = "cmd", args = { "cmd" } })
-	-- options.launch_menu = {
-	-- { label = "1PowerShell", shell() },
-	-- { label = "PowerShell Desktop", args = { "powershell" } },
-	-- { label = "Command Prompt", args = { "cmd" } },
-	-- { label = "Nushell", args = { "nu" } },
-	-- {
-	-- 	label = "Git Bash",
-	-- 	args = { }
-	-- },
-	-- }
-
-	-- elseif platform.is_mac then
-	-- 	options.default_prog = { "/opt/homebrew/bin/fish", "-l" }
-	-- 	options.launch_menu = {
-	-- 		{ label = "Bash", args = { "bash", "-l" } },
-	-- 		{ label = "Fish", args = { "/opt/homebrew/bin/fish", "-l" } },
-	-- 		{ label = "Nushell", args = { "/opt/homebrew/bin/nu", "-l" } },
-	-- 		{ label = "Zsh", args = { "zsh", "-l" } },
-	-- 	}
-	-- elseif platform.is_linux then
-	-- 	options.default_prog = { shell(), "-l" }
-	-- 	options.launch_menu = {
-	-- 		{ label = "Bash", args = { "bash", "-l" } },
-	-- 		{ label = "Fish", args = { "fish", "-l" } },
-	-- 		{ label = "Zsh", args = { "zsh", "-l" } },
-	-- 	}
+	-- table.insert(options.launch_menu, { label = "Git Bash", args = { } })
 end
 
 if platform.is_linux then
@@ -69,11 +51,16 @@ if platform.is_linux then
 	table.insert(options.launch_menu, { label = "Fish", args = { "fish", "-l" } })
 	table.insert(options.launch_menu, { label = "Zsh", args = { "zsh", "-l" } })
 	table.insert(options.launch_menu, { label = "Bash", args = { "bash", "-l" } })
-	table.insert(options.launch_menu, { label = "*** Elvish", args = { "elvish" } })
+	-- table.insert(options.launch_menu, { label = "*** Elvish", args = { "elvish" } })
 end
 
--- options.launch_menu = {
--- 	{ label = "NuShell", args = { "nu" } },
--- }
+-- if platform.is_mac then
+-- 	options.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+-- 	options.launch_menu = {
+-- 		{ label = "Bash", args = { "bash", "-l" } },
+-- 		{ label = "Fish", args = { "/opt/homebrew/bin/fish", "-l" } },
+-- 		{ label = "Nushell", args = { "/opt/homebrew/bin/nu", "-l" } },
+-- 		{ label = "Zsh", args = { "zsh", "-l" } },
+-- 	}
 
 return options
