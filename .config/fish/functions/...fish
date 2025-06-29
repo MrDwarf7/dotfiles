54f337a4f3a5dcad732831f873857718afe04e3e
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-06-29 15:15:43 +1000

    feat(fish): create recursive << .. >> function

--- .config/fish/functions/...fish
+++ .config/fish/functions/...fish
@@ -1,5 +1,13 @@
 #!/usr/bin/env fish
 
 function .. --wraps=source --description 'alias ..=cd ..'
-    cd .. $argv;
+    # echo "argv 1 : $argv[1]"
+    # echo "argv 2 : $argv[2]"
+    # echo "argv 3 : $argv[3]"
+    # echo "count val : $(count $argv)"
+
+    pushd .. 
+    if test (count $argv) -gt 0
+        .. $argv[2..-1]
+    end
 end
