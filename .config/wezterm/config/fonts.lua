00fb3980fe0d0b10fdabec4341613f2d240e2dc1
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-07-02 19:28:58 +1000

    style(wezterm): move font to variable

--- .config/wezterm/config/fonts.lua
+++ .config/wezterm/config/fonts.lua
@@ -3,15 +3,16 @@ local platform = require("utils.platform")
 
 -- local font = 'Maple Mono SC NF'
 -- local font_family = "JetBrainsMono Nerd Font"
-local font_size = 11.0
+local font_family = "FiraCode Nerd Font Mono"
+local font_size = 10.5
 -- platform.is_linux and 12.0 or 11.0
 
 return {
-	font = wezterm.font("Mononoki Nerd Font Mono", { weight = "Regular", italic = false }),
+	font = wezterm.font(font_family, { weight = "Regular", italic = false }),
 	font_size = font_size,
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0", "zero" },
 	-- There's a setting here for something along the lines of missing_unicode_fonts or similar we can set
-	warn_about_missing_glyphs = false,
+	warn_about_missing_glyphs = true,
 
 	text_background_opacity = 1.0, --0.6
 
