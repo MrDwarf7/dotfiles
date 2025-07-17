245e8e39ddc9fcfced3108384d62ce112fc362bf
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-07-17 14:45:45 +1000

    refactor(fish): sep. out mirror logic from update

--- .config/fish/functions/mirror_update.fish
+++ .config/fish/functions/mirror_update.fish
@@ -16,7 +16,7 @@
 #
 # Returns:
 #   $status ($status != 0, otherwise 0)
-function mirror_update
+function mirror_update --description 'Update the mirrorlist using rate-mirrors'
     if test -z "$PKG_MANAGER"
         printf "NOTE: PKG_MANAGER not set, defaulting to yay\n"
         set -l PKG_MANAGER "yay"
@@ -34,14 +34,15 @@ function mirror_update
 
     printf "Moving mirrorlist from %s to /etc/pacman.d/mirrorlist\n" $TMPFILE
     sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup || return 1
-    sudo mv $TMPFILE /etc/pacman.d/mirrorlist || return 1
-    sudo chown root:root /etc/pacman.d/mirrorlist                                   # Re-secure the mirrorlist file before leaving sudo
+    sudo mv $TMPFILE /etc/pacman.d/mirrorlist || return 2
+    sudo chown root:root /etc/pacman.d/mirrorlist || return 3                                   # Re-secure the mirrorlist file before leaving sudo
     # Make it readable by everyone
-    sudo chmod 644 /etc/pacman.d/mirrorlist
-
+    sudo chmod 644 /etc/pacman.d/mirrorlist || return 4
 
     ua_drop_caches
-    $PKG_MANAGER -Syyu --noconfirm || return $status
+
+    # _generic_update || return $status
+    # $PKG_MANAGER -Syyu --noconfirm || return $status
 
     # rate-mirrors --save=$TMPFILE arch --max-delay=21600
     # sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
@@ -66,7 +67,7 @@ end
 # Globals Variables:
 #   PKG_MANAGER (yay/paru)
 #
-function ua_drop_caches
+function ua_drop_caches --description 'Drop package manager and AUR caches'
     printf "Dropping caches\n"
     sudo true
     sudo paccache -rk3
--- .config/fish/functions/sysup.fish
+++ .config/fish/functions/sysup.fish
@@ -1,6 +1,50 @@
 #!/usr/bin/env fish
 
+# Runs a system update, optionally skipping certain parts
+#
+# Parameters:
+# Supports both `--skip` and `-s` flags to skip certain parts of the update.
+# - `m` for mirror update
+# - `r` for rustup update
+#
+# Usage:
+# sysup --skip m
+#
+# Returns:
+# Returns 0 on success, 1 on failure.
 function sysup --description 'System update function'
-    mirror_update || return $status
-    command rustup update
+    argparse --name=sysup 's/skip=' -- $argv
+    or return
+
+    set -l skip_mirror 1
+    set -l skip_rustup 1
+
+    # Loop here allows for supporting additional items in the switch/case later if wanted
+    if set -q _flag_skip 
+        for char in (string split '' $_flag_skip)
+            switch $char
+            case m
+                set skip_mirror 0
+            case r
+                set skip_rustup 0
+            case '*'
+                echo "Invalid skip value: $_flag_skip" >&2 
+                return 1
+            end
+        end
+    end
+
+    if test $skip_mirror -eq 1
+        mirror_update || return $status
+        _generic_update || return $status
+    end
+
+    if test $skip_rustup -eq 1
+        command rustup update
+    end
+
+    return $status
 end
+
+# mirror_update || return $status
+# command rustup update
