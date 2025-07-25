4fceeabda3eab2d8d562a5685a38ceb576d93987
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-07-25 14:48:16 +1000

    chore(zellij) update zel function

--- .config/fish/functions/zel.fish
+++ .config/fish/functions/zel.fish
@@ -1,5 +1,12 @@
 #!/usr/bin/env fish
 
 function zel --description 'zellij start'
-    command zellij --session $(tr -dc a-z0-9 </dev/urandom | head -c 3 ; printf "\n")
+
+    if test -z $argv
+        command zellij --session $(tr -dc a-z0-9 </dev/urandom | head -c 3 ; printf "\n") || return $status
+        return $status
+    end
+    command zellij $argv || return $status
+
+    return $status
 end
