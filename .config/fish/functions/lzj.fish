905f9844559fcaf3e6cc19104866145f3fd4c2a4
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-07-29 19:18:05 +1000

    feat: customize jj + update fish fnc

--- .config/fish/functions/lzj.fish
+++ .config/fish/functions/lzj.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/env fish
 
 function lzj
-    alias lzj jjui $argv
-    command jjui $argv
+    alias lzj jjui
+    if not set -q argv[1]
+        jjui -r 'all()'
+    else
+        jjui $argv
+    end
+
+    # command jjui -r 'all()' $argv
     # alias lzj lazyjj $argv
     # command lazyjj $argv
 end
