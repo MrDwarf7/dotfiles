08cdddf929aad841821f851b6d4524ab3f476f6b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-08-18 16:49:31 +1000

    ref(shellup): fix install shell script

--- .config/shellup/install.sh
+++ .config/shellup/install.sh
@@ -16,9 +16,9 @@ export BUILD_TYPE=release # release | debug
 export VERBOSE=1          # 1 | 0
 export INSTALL_PATH=/usr/local/bin
 
-install -m "$($INSTALL_MODE)" "$($TARGET)" "$($INSTALL_PATH)/$($PROJECT_NAME)" || {
+install -m "$INSTALL_MODE" "$TARGET" "$INSTALL_PATH/$PROJECT_NAME" || {
   echo "Installation failed. Please check the permissions and try again."
   exit 1
 }
 
-return $?
+exit 0
