30d29d6c51a7581be25487aa6a59c868536d333f
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-08-21 18:45:59 +1000

    fix(shellup): move behind interactive check

--- .config/fish/conf.d/zz_shell.fish
+++ .config/fish/conf.d/zz_shell.fish
@@ -1,11 +1,15 @@
 #!/usr/bin/env fish
 #
 
-# Should almost always be: 
+# Should almost always be:
 # /usr/local/bin/shellup
 
-set -l bin_path (which shellup 2>/dev/null)
+# Only run once we're actually using a terminal (Otherwise it runs on ly starting)
+if status is-interactive
+    set -l bin_path (which shellup 2>/dev/null)
 
-if test -n "$bin_path"
-    shellup &
+    if test -n "$bin_path"
+        shellup &
+    end
+    return 0
 end
