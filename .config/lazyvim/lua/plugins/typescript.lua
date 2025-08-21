4e4b69b2809703737b7c735ed077baa87211f945
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-08-21 18:45:59 +1000

    chore(vim): add typescript-tools.nvim plugin

---
+++ .config/lazyvim/lua/plugins/typescript.lua
@@ -0,0 +1,69 @@
+--- TSServer capabilities
+---@param opts? table
+---@param capabilities? table
+---@return table
+local function tsserv_cap(opts, capabilities)
+  opts = opts or {}
+  capabilities = capabilities or vim.lsp.protocol.make_client_capabilities()
+
+  return vim.tbl_deep_extend("force", capabilities, opts, {
+    includeCompletionsForImportStatements = true,
+    includeCompletionsWithSnippetText = true,
+    useLabelDetailsInCompletionEntries = true,
+
+    -- Params
+    includeInlayParameterNameHints = "all",
+    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+
+    -- Functions
+    includeInlayFunctionParameterTypeHints = true,
+    includeInlayFunctionLikeReturnTypeHints = true,
+    -- Variables
+    includeInlayVariableTypeHints = true,
+    includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+    -- Properties
+    includeInlayPropertyDeclarationTypeHints = true,
+    includeInlayEnumMemberValueHints = true,
+
+    quotePreference = "double",
+  })
+end
+
+return {
+  "pmizio/typescript-tools.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-lspconfig",
+  },
+  ft = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+  opts = function(_, opts)
+    opts = opts or {}
+    opts = vim.tbl_deep_extend("force", opts, {
+      settings = {
+        capabilities = tsserv_cap(opts),
+        -- capabilities,
+        root_dir = {
+          root = { "package.json " },
+          exclude = { "deno.json", "deno.jsonc" },
+        },
+        single_file_support = false,
+        tsserver_file_preferences = tsserv_cap(opts),
+        tsserver_plugins = {
+          "@types/node",
+          "@types/react",
+          "@types/react-dom",
+          "@types/w3c-web-serial",
+          "@types/web-bluetooth",
+          "@typescript-eslint/eslint-plugin",
+          "@typescript-eslint/parser",
+        },
+      },
+    })
+    return opts
+  end,
+  config = function(_, opts)
+    opts = opts or {}
+    require("typescript-tools").setup(opts)
+  end,
+}
