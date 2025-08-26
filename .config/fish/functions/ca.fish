e1d0f91e46e2635753a2356c03d502fb8ac00a36
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-08-26 11:43:24 +1000

    style(fish): cleanup listing functions (& logic)

--- .config/fish/functions/ca.fish
+++ .config/fish/functions/ca.fish
@@ -1,6 +1,12 @@
 #!/usr/bin/env fish
 
 function ca --description 'Clear screen and run ls'
-    command clear 
-    command eza -lah --color=always --follow-symlinks --icons=always --git
+    command clear
+    if test -z $LIST_CLIENT
+        printf "LIST_CLIENT is not set\n"
+        set -gx LIST_CLIENT ls
+        return 1
+    end
+    commandline -f repaint
+    command $LIST_CLIENT -lah --color=always --follow-symlinks --icons=always --git
 end
--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-function l 
+function l --description 'List all files including hidden files with details'
     if test -z $LIST_CLIENT
         printf "LIST_CLIENT is not set\n"
-        set -gx LIST_CLIENT "ls"
+        set -gx LIST_CLIENT ls
         return 1
     end
     command $LIST_CLIENT -lah --color=always --follow-symlinks --icons=always --git $argv
