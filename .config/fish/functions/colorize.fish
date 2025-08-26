8b42e0663ea465e4bc9a9450e1af0caf5652d41d
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-08-26 11:59:15 +1000

    docs(fish): document colorize fn properly

--- .config/fish/functions/colorize.fish
+++ .config/fish/functions/colorize.fish
@@ -1,9 +1,24 @@
 #!/usr/bin/env fish
 #
 
+# The actual colorize function that does the work
+# uses the associated helper function for usage info
+#
+# Parameters:
+# `argv[1]`: color name (e.g., red, green, blue)
+# `argv[2..-1]`: text to colorize (can be multiple words)
+#
+# Returns:
+# `1`: If color not found or error
+# `0`: If successful
 function colorize_handler --description 'Wrap text with ANSI color codes'
     set -l color_name $argv[1]
     set -l text $argv[2..-1] # Allow multiple words
+
+    # If the text already ends with a newline, don't add another
+    # set text (printf "%s" $text | tr -d '\n')
+
+    set text (string unescape $text | tr -d '\n')
 
     # Color mapping (like your hashmap)
     set -l color_code
@@ -44,22 +59,25 @@ function colorize_handler --description 'Wrap text with ANSI color codes'
             colorize red "Error: Color '$color_name' not found" >&2
             printf "\n"
             # printf "Error: Color '%s' not found\n\n" $color_name >&2
-            help_usage
+            colorize_help
             return 1
     end
 
     # Join text with spaces and wrap with color
     # printf "%s%s\e[0m" $color_code (string join ' ' $text)
     set -l full_text (string join ' ' $text)
+
     # printf "$color_code$full_text\033[0m"
     # printf "value of color_code: %s\n" $color_code
     # printf "%s%s\033[0m" $color_code $full_text
     echo -en "$color_code$full_text\033[0m"
+
     printf "\n"
+
     return 0
 end
 
-function help_usage --description 'Display usage information for colorize'
+function colorize_help --description 'Display usage information for colorize'
     set -l colors red green yellow blue magenta purple cyan white black gray grey bright_red bright_green bright_yellow bright_blue bright_magenta bright_purple bright_cyan bright_white
     printf "Usage: colorize <color> <text>\n"
     printf "Wraps the given text with ANSI color codes.\n\n"
@@ -72,14 +90,26 @@ function help_usage --description 'Display usage information for colorize'
 end
 
 # Convenience function for common usage
+# uses argparse for help flag
+#
+# Parameters:
+# `$argv[1]`?: color name (e.g., red, green, blue)
+# `$argv[2]`?: text to colorize (can be multiple words)
+#
+# Arguments:
+# `""`/null - Automatically parsed by argparse as help/usage flag
+# `-h/--help` - Show help message and exit
+#
+# Returns:
+# 0 on success
+# null on failure to parse args
+# status of colorize_handler (colore message)
 function colorize --description 'Alias for colorize'
     argparse h/help -- $argv
     or return
-
     if set -q _flag_help
-        help_usage
+        colorize_help
         return 0
     end
-
     colorize_handler $argv
 end
