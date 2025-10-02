c7882b19522f3e9b3597c14fd92594573fae568c
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-02 20:55:46 +1000

    chore/feat(nvim): copy over vscode_conf

---
+++ .config/lazyvim/lua/vscode_conf/actions.lua
@@ -0,0 +1,184 @@
+-- local vscode = require("vscode")
+print("Vscode specific actions file loads...")
+
+local function actions()
+  -- vim.keymap.set("n", "j", function()
+  -- 	require("vscode").call("editor.cursorDown")
+  -- end),
+  --
+  -- vim.keymap.set("n", "k", function()
+  -- 	require("vscode").call("editor.cursorDown")
+  -- end),
+
+  vim.keymap.set("i", "jj", function()
+    require("vscode").action("vscode-neovim.escape")
+  end)
+
+  vim.keymap.set("i", "kj", function()
+    require("vscode").action("vscode-neovim.escape")
+  end)
+
+  vim.keymap.set("i", "jk", function()
+    require("vscode").action("vscode-neovim.escape")
+  end)
+
+  vim.keymap.set("n", "gd", function()
+    print("Go to definition")
+    require("vscode").action("editor.action.revealDefinition")
+  end)
+
+  vim.keymap.set("n", "gD", function()
+    print("Go to declaration")
+    require("vscode").action("editor.action.revealDeclaration")
+  end)
+
+  vim.keymap.set("n", "<C-w>d", function()
+    print("Go to declaration")
+    require("vscode").action("editor.action.revealDefinitionAside")
+  end)
+
+  vim.keymap.set("n", "<C-w>e", function()
+    print("Reset editor window sizes")
+    require("vscode").action("workbench.action.evenEditorWidths")
+  end)
+
+  vim.keymap.set("n", "gr", function()
+    print("Go to references")
+    require("vscode").action("editor.action.goToReferences")
+  end)
+
+  vim.keymap.set("n", "gi", function()
+    print("Go to implementation")
+    require("vscode").action("editor.action.goToImplementation")
+  end)
+
+  vim.keymap.set("n", "gI", function()
+    print("Go to last edit location")
+    require("vscode").action("workbench.action.navigateToLastEditLocation")
+  end)
+
+  vim.keymap.set("n", "gt", function()
+    print("Go to type definition")
+    require("vscode").action("editor.action.goToTypeDefinition")
+  end)
+
+  vim.keymap.set("n", "gO", function()
+    print("Goto Symbol")
+    require("vscode").action("workbench.action.gotoSymbol")
+  end)
+
+  vim.keymap.set("n", "K", function()
+    print("Hover")
+    require("vscode").call("editor.action.showHover")
+  end)
+
+  vim.keymap.set("n", "gp", function()
+    print("Peek Declaration")
+    require("vscode").call("editor.action.peekDeclaration")
+  end)
+
+  vim.keymap.set("n", "<C-k>", function()
+    print("Peek definition")
+    require("vscode").action("editor.action.peekDefinition")
+  end)
+
+  -- Non Ctrl variants,
+  vim.keymap.set("n", "<Left>", function()
+    print("Increase view width")
+    require("vscode").call("workbench.action.increaseViewWidth")
+  end)
+
+  vim.keymap.set("n", "<Right>", function()
+    print("Decrease view width")
+    require("vscode").call("workbench.action.decreaseViewWidth")
+  end)
+
+  -- This are 'inverted' to an extent, because you're changing the actual editor size
+  vim.keymap.set("n", "<Down>", function()
+    print("Increase view height")
+    require("vscode").call("workbench.action.increaseViewHeight")
+  end)
+
+  vim.keymap.set("n", "<Up>", function()
+    print("Decrease view height")
+    require("vscode").call("workbench.action.decreaseViewHeight")
+  end)
+
+  -- Ctrl variants,
+  vim.keymap.set("n", "<C-Left>", function()
+    print("Increase view width")
+    require("vscode").call("workbench.action.increaseViewWidth")
+  end)
+
+  vim.keymap.set("n", "<C-Right>", function()
+    print("Decrease view width")
+    require("vscode").call("workbench.action.decreaseViewWidth")
+  end)
+
+  -- Ctrl variants,
+  vim.keymap.set("n", "<C-h>", function()
+    print("Increase view width")
+    require("vscode").call("workbench.action.increaseViewWidth")
+  end)
+
+  vim.keymap.set("n", "<C-l>", function()
+    print("Decrease view width")
+    require("vscode").call("workbench.action.decreaseViewWidth")
+  end)
+
+  vim.keymap.set("n", "<C-Down>", function()
+    print("Increase view size")
+    require("vscode").call("workbench.action.increaseViewSize")
+  end)
+
+  vim.keymap.set("n", "<C-Up>", function()
+    print("Decrease view size")
+    require("vscode").call("workbench.action.decreaseViewSize")
+  end)
+
+  vim.keymap.set("n", "<C-w>e", function()
+    print("Reset view size")
+    require("vscode").call("workbench.action.evenEditorWidths")
+  end)
+
+  vim.keymap.set("n", "<C-w>t", function()
+    print("Last editor closed")
+    require("vscode").call("workbench.action.reopenClosedEditor")
+  end)
+
+  vim.keymap.set("n", "<C-w>m", function()
+    print("Maxamize editor group")
+    require("vscode").call("workbench.action.minimizeOtherEditors")
+  end)
+
+  -- vim.keymap.set("n", "<Leader>tp", function()
+  -- 	print("Toggle problems panel")
+  -- 	require("vscode").action("workbench.actions.table.problems.view.viewAsTree")
+  -- end),
+  --
+  -- vim.keymap.set("n", "<Leader>tP", function()
+  -- 	print("Toggle problems panel")
+  -- 	require("vscode").action("workbench.actions.table.problems.view.viewAsTable")
+  -- end),
+
+  -- "binding": [
+  --     {
+  --         "key": "p",
+  --         "name": "Toggle problems panel view type",
+  --         "when": "editorTextFocus && neovim.mode != insert ",
+  --         "command": "workbench.actions.table.problems.view.viewAsTree",
+  --     },
+  --     {
+  --         "key": "P",
+  --         "name": "Toggle problems panel view type",
+  --         "when": "editorTextFocus && neovim.mode != insert ",
+  --         "command": "workbench.actions.table.problems.view.viewAsTable",
+  --     }
+  -- ]
+
+  -- actions(),
+end
+
+return {
+  actions(),
+}
---
+++ .config/lazyvim/lua/vscode_conf/keymaps.lua
@@ -0,0 +1,219 @@
+---@diagnostic disable: redundant-parameter
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local function keymaps()
+  local map = vim.keymap.set
+  local mdel = vim.keymap.del
+
+  -- local aufmt = require("utils.autoformatter")
+
+  -- Mimics the equiv. of
+  -- local silent_opts = { noremap = true, silent = true, desc = desc or "" }
+  -- making the desc optional in the process,
+  -- while keeping silent_opts both as a table AND callable as a fn
+
+  --- Default keymap options with silent and noremap settings already applied.
+  --- Can be used directly as a table or called with a description to create and return a new table.
+  ---@generic T: table
+  ---@generic R: PartiallyApplied<vim.keymap.set.Opts>|vim.keymap.set.Opts
+  ---@param opts? T|vim.keymap.set.Opts
+  ---@return R|PartiallyApplied<T>
+  local mod = function(opts)
+    ---@cast opts vim.keymap.set.Opts
+    opts = opts or { noremap = true, silent = true, desc = "" }
+
+    local mt = { --[[@as vim.keymap.set.Opts]]
+      __call = function(_, desc)
+        return { noremap = true, silent = true, desc = desc or "" }
+      end,
+    }
+    setmetatable(opts, mt)
+    return opts
+  end
+
+  --- Default keymapping options for noremap & silent
+  ---@type PartiallyApplied<vim.keymap.set.Opts>
+  local silent_opts = mod()
+
+  pcall(mdel, "n", "<Leader>r")
+  pcall(mdel, "n", "<Leader>rl")
+
+  -- Delete garbage window movement binds
+  pcall(mdel, "n", "<C-h>")
+  pcall(mdel, "n", "<C-j>")
+  pcall(mdel, "n", "<C-k>")
+  pcall(mdel, "n", "<C-l>")
+
+  -- Delete garbage buffer binds
+  pcall(mdel, "n", "<S-h>")
+  pcall(mdel, "n", "<S-l>")
+  pcall(mdel, "n", "[b")
+  pcall(mdel, "n", "]b")
+  pcall(mdel, "n", "<Leader>bb") -- Switch to Other Buffer
+  pcall(mdel, "n", "<Leader>`") -- Switch to Other Buffer
+
+  pcall(mdel, "n", "<Leader>bl") -- Delete Buffers to the Left
+  -- mdel("n", "<Leader>bp") -- Toggle pinned buffer
+  pcall(mdel, "n", "<Leader>bP") -- Delete non-pinned buffers
+  pcall(mdel, "n", "<Leader>br") -- Delete buffers to the Right
+
+  -- mdel("n", "<Leader>bd") -- Delete Buffer
+  -- mdel("n", "<Leader>bD") -- Delete Other Buffers
+  -- mdel("n", "<Leader>bo") -- Delete Other Buffers
+
+  -- Delete garbage menu bind
+  pcall(mdel, "n", "<Leader>l")
+
+  -- Delete garbage loc list
+  pcall(mdel, "n", "<Leader>xl")
+  ----- LSP UNBIND
+  -- Remove 'format' bind
+  pcall(mdel, { "n", "v" }, "<Leader>cf")
+  pcall(mdel, "n", "<Leader>cd")
+
+  -- UNBIND inlay hints
+  -- mdel("n", "<Leader>uh")
+
+  -- Remove random garbage quit command
+  pcall(mdel, "n", "<Leader>qq")
+
+  -- random garbage lazyvim changelog command?????????
+  pcall(mdel, "n", "<Leader>L")
+
+  pcall(mdel, "n", "<Leader>fT")
+  pcall(mdel, "n", "<Leader>ft")
+  pcall(mdel, "n", "<Leader>wd")
+  pcall(mdel, "n", "<Leader>wm")
+  pcall(mdel, "n", "<Leader>uZ")
+  pcall(mdel, "n", "<Leader>uz")
+
+  -- unbind snacks
+  pcall(mdel, "n", "<Leader>e")
+  pcall(mdel, "n", "<Leader>E")
+  pcall(mdel, "n", "<Leader>fn")
+
+  ---
+  --- Non-basics ---
+  ---
+
+  -- map("i", "jj", "<Esc>", silent_opts)
+  map("i", "jk", "<Esc>", silent_opts)
+  map("i", "kj", "<Esc>", silent_opts)
+  map("i", "kk", "<Esc>", silent_opts)
+
+  map("n", "<Leader>pl", "<CMD>Lazy<CR>", silent_opts("[l]azy"))
+
+  map("n", "<Leader>pe", "<CMD>LazyExtras<CR>", { desc = "Lazy Extras" })
+
+  map("n", "<Leader>pm", "<CMD>Mason<CR>", silent_opts("[m]ason"))
+  -- { noremap = true, silent = true, desc = "[m]ason" })
+
+  map("n", "H", "^", silent_opts) -- Shift + h (Or just H) to jump to start of line
+  map("n", "L", "$", silent_opts) -- Shift + l (Or just L) to jump to end of line
+
+  map("v", "H", "^", silent_opts) -- Shift + h (Or just H) to jump to start of line
+  map("v", "L", "$", silent_opts) -- Shift + l (Or just L) to jump to end of line
+
+  map("n", "y<S-h>", "y^", silent_opts) -- Same as above for yanking
+  map("n", "y<S-l>", "y$", silent_opts) -- Same as above for yanking
+
+  map("n", "d<S-h>", "d^", silent_opts) -- Same as above for yanking
+  map("n", "d<S-l>", "d$", silent_opts) -- Same as above for yanking
+
+  map("n", "<C-w>e", "<C-w>=", silent_opts("[e]qualize")) -- ctrl + w + = : easier to hit to equalize the width of buffers
+
+  map("v", "p", '"_dP', { noremap = true, silent = true })
+
+-- stylua: ignore start
+-- if vim.lsp.inlay_hint then require("snacks").toggle.inlay_hints():map("<Leader>ti") end
+	map("n", "<Leader>lf", function() LazyVim.format({ force = true }) end, { desc = "Format" })
+	map("n", "<Leader>ld", vim.diagnostic.open_float, { desc = "Line Diagnostic" })
+  -- stylua: ignore end
+
+  map("n", "]b", "<cmd>bnext<cr>", { desc = "[n]ext" })
+  map("n", "[b", "<cmd>bprevious<cr>", { desc = "[p]revious" })
+
+  map("n", "<Leader>bN", "<cmd>enew<cr>", { desc = "New File" })
+
+  map("n", "<Leader>bn", ":bnext<CR>", silent_opts("[n]ext"))
+  map("n", "<Leader>bp", ":bprev<CR>", silent_opts("[p]revious"))
+
+  map("n", "<Leader>b]", ":bnext<CR>", silent_opts("[n]ext"))
+  map("n", "<Leader>b[", ":bprev<CR>", silent_opts("[p]revious"))
+
+  map("n", "<Left>", ":vertical resize +2<CR>", silent_opts)
+  map("n", "<Right>", ":vertical resize -2<CR>", silent_opts)
+
+  map("n", "<C-h>", ":vertical resize +2<CR>", silent_opts)
+  map("n", "<C-l>", ":vertical resize -2<CR>", silent_opts)
+  map("n", "<Down>", ":resize -2<CR>", silent_opts)
+  map("n", "<Up>", ":resize +2<CR>", silent_opts)
+
+  map("n", "<Leader>e", "<cmd>Oil<CR>", { desc = "Oily" })
+-- map("n", "<Leader>bc", function()
+--   Snacks.bufdelete()
+-- end, silent_opts, { desc = "[X]close" })
+
+-- map("n", "<Leader>x", function()
+--   Snacks.bufdelete()
+-- end, silent_opts, { desc = "[X]close" })
+
+-- map("n", "<Leader><S-x>", function()
+--   Snacks.bufdelete().all(_)
+-- end, silent_opts, { desc = "[X]close" })
+
+-- a sort of 'reset' to baseline' function
+-- map({ "n", "v" }, "<Leader>l.", function()
+--   aufmt:formatters_reset()
+--   vim.print("Autoformatting has been reset to default settings.")
+-- end, silent_opts("[.]All formatters on"))
+--
+-- map({ "n", "v" }, "<Leader>lx", function()
+--   aufmt:fmt_quit_global()
+-- end, silent_opts("[x](Global) Quit & No fmt"))
+--
+-- map({ "n", "v" }, "<Leader>lX", function()
+--   aufmt:fmt_quit_buf()
+-- end, silent_opts("[X](Buf) Quit & No fmt"))
+
+-- Longer maps with function calls --
+-- stylua: ignore start
+map("n", "<Leader>p<S-l>", function() LazyVim.news.changelog() end, { desc = "LazyVim Changelog" })
+
+map("n", "]t", function() require("todo-comments").jump_next() end, silent_opts("Next [t]odo"))
+map("n", "[t", function() require("todo-comments").jump_next() end, silent_opts("Next [t]odo"))
+map("n", "<Leader>ft", ":lua Snacks.picker.todo_comments()<CR>", silent_opts("[F]ind [T]odo's"))
+map('n', "<Leader>ft", ":lua Snacks.picker.todo_comments({ keywords = { 'TODO', 'FIX', 'FIXME', 'NOTE', 'IMP' } })<CR>", silent_opts("[F]ind [T]odo's" ))
+  -- stylua: ignore end
+
+  -- local user_cmd = require("config.user_commands")
+  -- user_cmd.sudo_save()
+
+  local gen_utils = require("utils.generic")
+
+  map("n", "<Leader>W", function()
+    gen_utils.sudo_write()
+  end, silent_opts("[W]rite with sudo"))
+
+  map("n", "<Leader>go", function()
+    vim.cmd([[ :Octo ]])
+  end, silent_opts("[G]ithub [O]cto"))
+
+  ------@see _ lua/plugins/marks.lua:35
+  ---local docs = {}
+  -- map("n", "[m", function()
+  --   local marks = require("marks")
+  --   marks.prev()
+  -- end)
+  --
+  -- map("n", "]m", function()
+  --   local marks = require("marks")
+  --   marks.next()
+  -- end)
+end
+
+return {
+  keymaps(),
+}
