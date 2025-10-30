a41d70162c70d8feb870d4a847ac2c97aef90bdf
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-30 20:32:13 +1100

    chore(usr/local/bin): not req. libhyprutils.so.*

--- usr/local/bin/libhyprutils-symlink.sh
+++ usr/local/bin/libhyprutils-symlink.sh
@@ -5,10 +5,10 @@
 
 LIB_DIR="/usr/lib"
 TARGET_LIB=$(ls -v "$LIB_DIR"/libhyprutils.so.0.[0-9]* 2>/dev/null | tail -n 1) # Get highest versioned
-TARGET_CREATE="libhyprutils.so.8"
+TARGET_CREATE="libhyprutils.so.9"
 
 if [ -n "$TARGET_LIB" ] && [ ! -e "$LIB_DIR/$TARGET_CREATE" ]; then
-  ln -sf "$TARGET_LIB" "$LIB_DIR/$TARGET_CREATE"
+  sudo ln -sf "$TARGET_LIB" "$LIB_DIR/$TARGET_CREATE"
   echo "Created symlink $LIB_DIR/$TARGET_CREATE -> $TARGET_LIB"
 elif [ -e "$LIB_DIR/$TARGET_CREATE" ]; then
   echo "Symlink already exists: $LIB_DIR/$TARGET_CREATE"
