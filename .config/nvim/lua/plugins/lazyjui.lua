b3996354115ef7c3eed5eb31c43eacfb40be4e8d
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-30 20:32:13 +1100

    chore(hypr): update workspace assign. for apps

--- .config/nvim/lua/plugins/lazyjui.lua
+++ .config/nvim/lua/plugins/lazyjui.lua
@@ -1,23 +1,29 @@
 -- Inspired by - "swaits/lazyjj.nvim" and the lazygit.nvim plugin
 return {
-  "mrdwarf7/lazyjui.nvim",
-  lazy = true,
-  -- dev = true,
-  dependencies = "nvim-lua/plenary.nvim",
-  keys = {
+	-- -- from production branch --
+	-- "mrdwarf7/lazyjui.nvim",
+	-- dev = false,
+
+	-- -- from local FS / Dev --
+	"lazyjui.nvim",
+	dev = true,
+
+	lazy = true,
+	dependencies = "nvim-lua/plenary.nvim",
+	keys = {
     -- stylua: ignore start
     { "<Leader>aa", function() require("lazyjui").open() end, desc = "LazyJui [a]ll" },
-    -- stylua: ignore end
-  },
-  ---@type lazyjui.Opts
-  opts = {
-    cmd = { "jjui", "-r", "all()" },
-    winblend = 0,
-  },
-  -- config = function(_, opts)
-  --   require("lazyjui").setup({
-  --     cmd = { "jjui", "-r", "all()" },
-  --     winblend = 20,
-  --   })
-  -- end,
+		-- stylua: ignore end
+	},
+	---@type lazyjui.Opts
+	opts = {
+		cmd = { "jjui", "-r", "all()" },
+		winblend = 0,
+	},
+	-- config = function(_, opts)
+	--   require("lazyjui").setup({
+	--     cmd = { "jjui", "-r", "all()" },
+	--     winblend = 20,
+	--   })
+	-- end,
 }
