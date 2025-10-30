aaed599a9ea66b6957cc5df6da36fbef51f0327b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-30 20:32:13 +1100

    chore(hypr+fish): add davinci-control-panels-setup for hypr+fish

--- .config/fish/functions/davinci-control-panels-setup.fish
+++ .config/fish/functions/davinci-control-panels-setup.fish
@@ -1,19 +1,19 @@
 #!/usr/bin/env fish
 #
 
-function davinci-resolve --wraps=source --description 'Launch DaVinci Resolve'
+function davinci-control-panels-setup --wraps=source --description 'Launch DaVinci Resolve Control Panels Setup'
     # set -l qt_qpa_kv (env | grep ^QT_QPA_PLATFORM=)
     set -l qt_key_name QT_QPA_PLATFORM
     set -l qt_key_value xcb
 
-    set -l davinci_exec /opt/resolve/bin/resolve
+    set -l davinci_control_panels $(command -v davinci-control-panels-setup)
 
     # Handle the fact that DR uses python
     if 00valid_pacman mise
         mise deactivate
     end
 
-    eval "$qt_key_name=$qt_key_value $davinci_exec $argv &>/dev/null &; disown"
+    eval "$qt_key_name=$qt_key_value $davinci_control_panels $argv &>/dev/null &; disown"
     commandline -f repaint
     01eval_if_pacman mise "mise activate fish | source"
     return 0
