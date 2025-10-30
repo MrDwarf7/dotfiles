746b1840a9e6048bfcc80aa57674a64fa89b5249
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-10-30 20:32:13 +1100

    chore(nvim): add ufo.nvim plugin

---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,17 @@
+---@type LazyPluginBase
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  event = "LspAttach",
+  keys = {
+    -- stylua: ignore start
+    { "zR", function() require("ufo").openAllFolds() end,         desc = "UFO: Open All Folds" },
+    { "zM", function() require("ufo").closeAllFolds() end,        desc = "UFO: Close All Folds" },
+    { "zr", function() require("ufo").openFoldsExceptKinds() end, desc = "UFO: Open Folds Except Kinds" },
+    { "zm", function() require("ufo").closeFoldsWith() end,       desc = "UFO: Close Folds With" },
+    -- stylua: ignore end
+  },
+  opts = {},
+}
