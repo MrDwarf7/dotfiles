d9e20b96c3e73678566769945cf67951e6b19a88
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-12-21 20:16:05 +1100

    chore(nvim): update lazyjui.nvim configuration

--- .config/nvim/lua/plugins/lazyjui.lua
+++ .config/nvim/lua/plugins/lazyjui.lua
@@ -3,8 +3,7 @@
 return {
   -- -- from github pull -- --
   "mrdwarf7/lazyjui.nvim",
-  branch = "dev",
-  dev = false,
+  -- branch = "dev",
 
   -- -- from local FS / Dev --
   -- "lazyjui.nvim",
@@ -21,8 +20,13 @@ return {
   },
   ---@type lazyjui.Opts
   opts = {
+    -- Optionally:
+    border_chars = {},
+    -- { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
     cmd = { "jjui", "-r", "all()" },
-    winblend = 0,
+    height = 0.8, -- default is 0.8,
+    width = 0.7, -- default is 0.9,
+    winblend = 0, -- default is 0 (fully opaque). Set to 100 for fully transparent (not recommended though).
   },
   -- config = function(_, opts)
   --   require("lazyjui").setup({
