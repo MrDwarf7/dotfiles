fa2fac84d81b818339b81345bde726cb6fb658a0
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-12-28 13:56:46 +1100

    chore(nvim): add luau support

--- .config/nvim/lua/plugins/lazyjui.lua
+++ .config/nvim/lua/plugins/lazyjui.lua
@@ -2,13 +2,13 @@
 ---@type LazyPluginBase
 return {
   -- -- from github pull -- --
-  "mrdwarf7/lazyjui.nvim",
+  -- "mrdwarf7/lazyjui.nvim",
   -- branch = "dev",
 
   -- -- from local FS / Dev --
-  -- "lazyjui.nvim",
-  -- branch = "dev",
-  -- dev = true,
+  "lazyjui.nvim",
+  branch = "dev",
+  dev = true,
 
   lazy = true,
   dependencies = "nvim-lua/plenary.nvim",
@@ -21,8 +21,9 @@ return {
   ---@type lazyjui.Opts
   opts = {
     -- Optionally:
-    border_chars = {},
-    -- { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    -- border_chars = {},
+    -- border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    -- border_thickness = 2,
     cmd = { "jjui", "-r", "all()" },
     height = 0.8, -- default is 0.8,
     width = 0.7, -- default is 0.9,
