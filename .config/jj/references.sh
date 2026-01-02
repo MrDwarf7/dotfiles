31d13c6cf43be9cc3472c4fb2570884822917bd5
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-01-02 20:23:32 +1100

    chore(jj): update the `refaa` command data

--- .config/jj/references.sh
+++ .config/jj/references.sh
@@ -26,14 +26,22 @@
 #
 # }
 
+# The total lines to clip AFTER the searched term
+TOTAL_LINES_AFTER=37
+TOTAL_LINES_BEFORE=1
+SEARCHED_CONTEXT_LINES=1
+SEARCHED_TAG_PREFIX="REF S"
+
+RG_PREF_FLAGS="--color=always"
+
 function help_refs {
-  cat <<EOF
+  cat <<'EOF'
 
 Usage: references.sh [search terms]
 Output the 'REF' block inside the jj config file.
 Passing a single word search term will filter the references to only those that match the term.
 
-Default (surrounding) \`context\`  via the \`-C\` flag is \`1\`.
+Default (surrounding) `context` via the `-C` flag is `1`.
 This means 1 line above, and 1 below the search term/result.
 
 If no search terms are provided, all references will be shown.
@@ -53,6 +61,7 @@ function main {
   current_file_name=$(basename "$current_file_path")
   current_file_parent_dir=$(dirname "$current_file_path")
 
+  # we have help flags!
   case "${1:-}" in
   h | -h | --help)
     help_refs
@@ -64,16 +73,16 @@ function main {
   # and store the text in a variable
 
   declare -a search_terms=() # Not sure we even need this tbh
+  declare output=""          # output buffer
 
   if [ "$#" -gt 0 ]; then
     search_terms+=("$@")
-    output=$(rg -g "!$current_file_name" "REF S" "$current_file_parent_dir" -INA 34)
-    output=$(printf "%s\n" "$output" | rg -i "${search_terms[@]}" -C 1)
-    printf "%s\n" "$output"
-    return 0
+    output=$(rg -g "!$current_file_name" "${SEARCHED_TAG_PREFIX}" "$current_file_parent_dir" -INA $TOTAL_LINES_AFTER -B $TOTAL_LINES_BEFORE)
+    output=$(printf "%s\n" "$output" | rg $RG_PREF_FLAGS -i "${search_terms[@]}" -C $SEARCHED_CONTEXT_LINES)
+  else
+    output=$(rg $RG_PREF_FLAGS -g "!$current_file_name" "${SEARCHED_TAG_PREFIX}" "$current_file_parent_dir" -INA $TOTAL_LINES_AFTER -B $TOTAL_LINES_BEFORE)
   fi
 
-  output=$(rg -g "!$current_file_name" "REF S" "$current_file_parent_dir" -INA 34)
   printf "%s\n" "$output"
   return 0
 }
