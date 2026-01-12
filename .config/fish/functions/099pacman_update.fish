f297f84fd67d28247e2a9f694c8e1a96d6b4785e
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-01-12 15:18:35 +1100

    refactor(fish): split sysup sub-calls

---
+++ .config/fish/functions/099pacman_update.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+#
+
+function 099pacman_update --description 'Uses `pacman` to update official packages. Ideally run prior to AUR updates. Wrapped with sudo "on" calls'
+    if not 00valid_pacman pacman
+        colorize red "pacman command not found. This function requires pacman to update official packages.\n"
+        return 1
+    end
+
+    sudo true
+    command sudo pacman -Syu --needed --noconfirm || return $status
+    sudo true
+    return $status
+end
--- .config/fish/functions/sysup.fish
+++ .config/fish/functions/sysup.fish
@@ -7,12 +7,14 @@ set k_skip s
 set k_mirror m
 set k_rustup r
 set k_packages p
+set k_shutdown d
 
 
 function __sysup_cmp
     complete -c sysup -s $k_help -l help -d 'Show this help message and exit'
     complete -c sysup -s $k_skip -l skip -a "m r p" -d 'Skip certain parts of the update. Use with m (mirror), r (rustup), p (packages)'
     complete -c sysup -l skip -a "m r p" -d 'Skip certain parts of the update. Use with m (mirror), r (rustup), p (packages)'
+    complete -c sysup -s $k_shutdown -l shutdown -d 'Shutdown the system after update completes. Only works if the update completes successfully.'
     return 0
 end
 
@@ -42,6 +44,7 @@ Includes:
 ,                    ,                           ,           ,
 ,-$k_help            , --help                    ,# Show this help message and exit,
 ,-$k_skip            , --skip                    ,# Skip certain parts of the update. Use with m (mirror) | r (rustup) | p (packages),
+,-$k_shutdown        , --shutdown                ,# Shutdown the system after update completes. Only works if the update completes successfully.,
 " "
 ,                                       ,           ,,
 ,Command                                ,Description,,
@@ -51,6 +54,7 @@ Includes:
 ,sysup -$k_skip -m                        ,# Use -m to skip mirror update,,
 ,sysup -$k_skip -r                        ,# Use -r to skip rustup update,,
 ,sysup -$k_skip -p                        ,# Use -p to skip package update,,
+,sysup -$k_shutdown                       ,# Shutdown the system after successful update,,
 "
     return 0
 end
@@ -70,7 +74,7 @@ end
 function sysup --description 'System update function'
     __sysup_cmp
 
-    argparse --name=sysup 's/skip=' h/help -- $argv
+    argparse --name=sysup $k_help/help $k_shutdown/shutdown $k_skip/skip= -- $argv
     or return
 
     if set -q _flag_help
@@ -82,6 +86,9 @@ function sysup --description 'System update function'
     set -l skip_mirror false
     set -l skip_rustup false
     set -l skip_packages false
+
+    set -l shutdown_waittime_mins 1
+    set -l shutdown_waittime_secs (math "60 * $shutdown_waittime_mins")
 
     # Loop here allows for supporting additional items in the switch/case later if wanted
     if set -q _flag_skip
@@ -103,40 +110,59 @@ function sysup --description 'System update function'
     set -l mise_exists (test (command -v mise); and echo 0; or echo 1)
 
     if test $mise_exists -eq 0
-        printf "Disabling mise...\n"
+        colorize yellow "[SYSUP] Disabling mise...\n\n"
         mise deactivate 2>&1 >/dev/null
     else
-        printf "mise not found, skipping deactivation.\n"
+        colorize yellow "[SYSUP] mise not found, skipping deactivation.\n\n"
     end
 
-    colorize blue "\n\nStarting system update...\n"
+    colorize blue "[SYSUP] Starting system update...\n"
 
     if test $skip_mirror = true
-        colorize yellow "Skipping mirror update...\nRunning generic update only...\n"
-        099generic_update || return $status
+        colorize yellow "[SYSUP] Skipping mirror update...\n"
+        colorize yellow "[SYSUP] Running generic update only...\n"
+        099pacman_update || return $status
+        099aur_update || return $status
     else if test $skip_packages = true
-        colorize yellow "Skipping package update...\nRunning mirror update only...\n"
+        colorize yellow "[SYSUP] Skipping package update...\n"
+        colorize yelloe "[SYSUP] Running mirror update only...\n"
         mirror_update || return $status
     else
-        colorize yellow "Running full update...\n"
+        colorize yellow "[SYSUP] Running full update...\n"
         mirror_update
-        099generic_update
+        099pacman_update || return $status
+        099aur_update || return $status
         099generic_cache_drop || return $status
     end
 
     if test $skip_rustup != true
-        colorize yellow "Updating rustup...\n"
+        colorize yellow "[SYSUP] Updating rustup...\n"
         command rustup update || return $status
     end
 
     if test $mise_exists -eq 0
-        printf "Re-enabling mise...\n"
+        colorize yellow "[SYSUP] Re-enabling mise...\n"
         mise activate fish | source
+        colorize yellow "[SYSUP] Running `mise up` to update tools\n"
+        mise up || return $status
     else
         printf "mise not found, skipping re-activation.\n"
     end
 
-    colorize green "System update complete!\n"
+    colorize green "[SYSUP] System update complete!\n"
+
+
+    if set -q _flag_shutdown
+        colorize yellow "[SYSUP] Shutting down system in $shutdown_waittime_mins minute(s). Press Ctrl-C to cancel.\n"
+
+        while test $shutdown_waittime_secs -gt 0
+            colorize yellow "[SYSUP] System will shutdown in $shutdown_waittime_secs seconds...\r"
+            sleep 1
+            set shutdown_waittime_secs (math $shutdown_waittime_secs - 1)
+        end
+        # colorize red "[SYSUP] THIS IS DEBUG MODE!!!!\n"
+        sudo shutdown -h now
+    end
 
     return $status
 end
