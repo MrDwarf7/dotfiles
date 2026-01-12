639c747cecbe4a5e5cc668df85c7c230027cbf45
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-01-12 14:37:31 +1100

    chore(nvim): remove dead code for vimpack

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,9 +8,6 @@ require("config.autocmds")
 ---@diagnostic disable-next-line: unused-local
 local utils = require("utils")
 
---- nope, nvm
--- require("vimpack")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
