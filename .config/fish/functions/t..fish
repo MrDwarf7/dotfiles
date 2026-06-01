72a0c3e43a5064fd7352b1dce556edb354e12c00
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-06-01 14:42:38 +1000

    feat(fish): add thunar launch cmd to fish

---
+++ .config/fish/functions/t..fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+#
+
+function t. --wraps=source --description 'Wrapper around Thunar to launch in the current directory'
+    set -l all_args (string join '.' $argv)
+    if 00valid_pacman thunar
+        thunar $all_args &>/dev/null &
+        return 0
+    end
+    return 1
+end
