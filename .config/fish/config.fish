abb75fd2216109b002c6ce81b2d560ec4038cb40
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-07-01 00:04:13 +1000

    chore(fish): add misc root config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,14 @@
 
 # Commands to run in interactive sessions can go in here
 if status is-interactive
+    # 01eval_if_pacman zoxide "zoxide init fish" # zoxide init fish | source
+    # 01eval_if_pacman fzf "fzf --fish" # fzf --fish | source
+    # #
+    # 04var_to_syspath mise "$HOME/.xdg/data/mise/shims" --prepend
+    # 01eval_if_pacman mise "mise activate fish | source"
+
     # Immediately call starship on term start (interactive)
-    01eval_if_pacman starship "starship init fish | source"
+    01eval_if_pacman starship "starship init fish --print-full-init"
     commandline -f repaint
     # 01eval_if_pacman direnv "direnv hook fish | source"
 
